3a38ebf75e945f49d25790b4a2965d5c49da672f
niche0905 <yudle537@gmail.com>
2025-07-03 21:30:56 +0900

    pch 작업
    간단한 pch 작업과 사용할 가능성이 있는 Container 들을 미리 추가하였다

---
+++ ServerEngine/Container.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <array>
+#include <vector>
+#include <list>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
---
+++ ServerEngine/CorePch.h
@@ -0,0 +1,12 @@
+#pragma once
+
+// TODO: 필요한 것들 추가
+#include "Container.h"
+
+#include <Windows.h>
+#include <iostream>
+
+#include <WinSock2.h>
+#include <MSWSock.h>
+#include <WS2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
