fcf469110150372e5ed407d1fbd193a5bff50a72
niche0905 <yudle537@gmail.com>
2025-09-10 18:21:25 +0900

    IoObject 추가
    IoCore를 통해서 I/O 작업을 수행하는 주체를 인터페이스 형태로 선언하였다

---
+++ ServerEngine/IoObject.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "pch.h"
+
+/*-------------
+   IoObject
+-------------*/
+//
+// IoObject는 비동기 입출력의 주체입니다
+// 인터페이스 형태로 GetHandle()과 Dipatch()를 제공해야 합니다
+//
+
+class IoObject : public std::enable_shared_from_this<IoObject>
+{
+public:
+	virtual HANDLE GetHandle() abstract;
+	virtual void Dispatch(class IoEvent* ioEvent, int32 numOfBytes = 0) abstract;
+
+};
+
