1e43afc730b7136eb88e0cf33cd839433dcffe05
niche0905 <yudle537@gmail.com>
2025-09-10 15:25:35 +0900

    IocpCore 초안
    IocpCore의 초안을 작성하였지만 입력할 수 없는 부분이 있어서 미뤄두었다

---
+++ ServerEngine/IocpCore.cpp
@@ -0,0 +1,31 @@
+#include "pch.h"
+#include "IocpCore.h"
+
+bool IocpCore::Initialize()
+{
+	iocpHandle_ = ::CreateIoCompletionPort(INVALID_HANDLE_VALUE, nullptr, 0, 0);
+	return (iocpHandle_ != INVALID_HANDLE_VALUE);
+}
+
+void IocpCore::Terminate()
+{
+	::CloseHandle(iocpHandle_);
+}
+
+bool IocpCore::Dispatch(DWORD timeoutMs)
+{
+	// TODO: Seession과 IoObject가 구현이 되어야 구현 가능
+
+	return false;
+}
+
+bool IocpCore::AttachSession(Session& session)
+{
+	//return ::CreateIoCompletionPort()
+	return false;
+}
+
+void IocpCore::DetachSession(Session& session)
+{
+
+}
---
+++ ServerEngine/IocpCore.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "IoCore.h"
+
+/*-------------
+   IocpCore
+-------------*/
+//
+// Windows의 IOCP 방식을 사용하는 Core 입니다
+//
+
+class IocpCore : public IoCore
+{
+public:
+	IoBackend Backend() const noexcept override { return IoBackend::IOCP; }
+
+	bool Initialize() override;
+	void Terminate() override;
+
+	bool Dispatch(DWORD timeoutMs = INFINITE) override;
+
+	bool AttachSession(Session& session) override;
+	void DetachSession(Session& session) override;
+
+private:
+	HANDLE iocpHandle_{ INVALID_HANDLE_VALUE };
+
+};
+
