5eaf9cee0cbb75edaa60b2f8d56082ec2ebac4fb
niche0905 <yudle537@gmail.com>
2025-09-14 16:54:11 +0900

    Buffer 추가
    byte(uint8, == signed char)의 Type을 기본으로 하는 Buffer를 using을 통해 정의하였다

---
+++ ServerEngine/Buffer.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "BaseBuffer.h"
+
+/*----------
+   Buffer
+----------*/
+//
+// Buffer는 BYTE형식의 데이터를 담는 그릇입니다
+// 
+
+using Buffer = BaseBuffer<byte>;
+
+// TODO: 추후에 공통된 추가 메서드가 필요하면 class 정의
+//class Buffer : public BaseBuffer<byte>
+//{
+//	...
+//};
