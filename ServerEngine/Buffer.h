c815680532015d7bd06765c434d2007e14ea1da5
niche0905 <yudle537@gmail.com>
2025-09-14 17:08:54 +0900

    Buffer 정의 변경
    Buffer 클래스를 BaseBuffer<byte>를 상속 받는 것으로 만들어 RecvBuffer로 사용하기 위해 필요하다고 생각되는 공통 메서드를 추가하였다

--- ServerEngine/Buffer.h
+++ ServerEngine/Buffer.h
@@ -8,10 +8,26 @@
 // Buffer는 BYTE형식의 데이터를 담는 그릇입니다
 // 
 
-using Buffer = BaseBuffer<byte>;
+class Buffer : public BaseBuffer<byte>
+{
+protected:
+	inline static constexpr size_t BUFFER_COUNT = 10;
 
-// TODO: 추후에 공통된 추가 메서드가 필요하면 class 정의
-//class Buffer : public BaseBuffer<byte>
-//{
-//	...
-//};
+public:
+	// 호출자가 제공하는 WSABUF[2]를 채워줍니다
+	// 반환값은 WSABUF에 채워진 개수입니다 (0 ~ 2)
+	virtual DWORD PrepareRecv(WSABUF (&wsabuf)[2]) noexcept abstract;
+	
+	// WSABUF에서 사용된 만큼 데이터를 버퍼에 커밋합니다
+	virtual void Commit(size_t nums) noexcept abstract;
+
+	// 파서가 버퍼에 담긴 데이터를 제거하지 않고 읽기만 합니다
+	virtual std::pair<const byte*, size_t> Peek() const noexcept abstract;
+
+	// 파서가 버퍼에 담긴 데이터를 제거합니다
+	virtual void Consume(size_t nums) noexcept abstract;
+
+	// 버퍼에 남은 여유 공간의 크기를 반환합니다
+	virtual size_t FreeSize() const noexcept abstract;
+
+};
