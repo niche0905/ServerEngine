a253e7aedcff61165cb8883a8443ceabde559d10
niche0905 <yudle537@gmail.com>
2025-09-14 17:22:35 +0900

    위치 정렬

--- ServerEngine/RecvBuffer.h
+++ ServerEngine/RecvBuffer.h
@@ -17,18 +17,18 @@ protected:
 public:
 	// 호출자가 제공하는 WSABUF[2]를 채워줍니다
 	// 반환값은 WSABUF에 채워진 개수입니다 (0 ~ 2)
-	virtual DWORD PrepareRecv(WSABUF (&wsabuf)[2]) noexcept abstract;
+	virtual DWORD								PrepareRecv(WSABUF (&wsabuf)[2]) noexcept abstract;
 	
 	// WSABUF에서 사용된 만큼 데이터를 버퍼에 커밋합니다
-	virtual void Commit(size_t nums) noexcept abstract;
+	virtual void								Commit(size_t nums) noexcept abstract;
 
 	// 파서가 버퍼에 담긴 데이터를 제거하지 않고 읽기만 합니다
-	virtual std::pair<const byte*, size_t> Peek() const noexcept abstract;
+	virtual std::pair<const byte*, size_t>		Peek() const noexcept abstract;
 
 	// 파서가 버퍼에 담긴 데이터를 제거합니다
-	virtual void Consume(size_t nums) noexcept abstract;
+	virtual void								Consume(size_t nums) noexcept abstract;
 
 	// 버퍼에 남은 여유 공간의 크기를 반환합니다
-	virtual size_t FreeSize() const noexcept abstract;
+	virtual size_t								FreeSize() const noexcept abstract;
 
 };
