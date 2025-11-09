6adefc6b2c8a4aa0c27f2946191ea6d0def78869
niche0905 <yudle537@gmail.com>
2025-11-09 16:50:27 +0900

    IoCore 주석 추가
    IoCore에 주석을 적어 가상 메서드들의 용도를 설명함

--- ServerEngine/IoCore.h
+++ ServerEngine/IoCore.h
@@ -19,13 +19,17 @@ public:
 	IoCore();
 	~IoCore();
 
+	// IoBackend를 반환하는 순수 가상 함수 (IOCP 또는 RIO)
 	virtual IoBackend Backend() const noexcept = 0;
 
+	// 초기화 및 종료를 위한 순수 가상 함수
 	virtual bool Initialize() = 0;
 	virtual void Terminate() = 0;
 
+	// 이벤트 디스패치를 위한 순수 가상 함수 (IO 이벤트 처리)
 	virtual bool Dispatch(DWORD timeoutMs = INFINITE) = 0;
 	
+	// 세션을 IoCore에 연결 및 해제하는 순수 가상 함수
 	virtual bool AttachSession(Session& session) = 0;
 	virtual void DetachSession(Session& session) = 0;
 
