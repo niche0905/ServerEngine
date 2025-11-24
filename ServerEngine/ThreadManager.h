2d9dc52a6fd7c20424caf6c99aede27df959f905
niche0905 <yudle537@gmail.com>
2025-11-24 21:28:35 +0900

    TM 주석 추가
    TM 주석에 내용을 추가하고
    각 메서드에 간단한 설명을 추가하였다

--- ServerEngine/ThreadManager.h
+++ ServerEngine/ThreadManager.h
@@ -5,6 +5,13 @@
 -----------------*/
 //
 // ThreadManager는 thread 관리를 담당합니다
+// 
+// 사용 예시
+// 전역 변수로 접근
+// GThreadManager.Launch([]()
+// {
+//    do something...
+// });
 //
 
 class ThreadManager
@@ -14,13 +21,18 @@ public:
 	~ThreadManager();
 
 public:
+	// 새로운 thread 생성 및 실행
 	void Launch(std::function<void()> func);
+	// 모든 thread 종료 대기
 	void Join();
 
+	// 각자 thread에서 처리할 작업
 	static void DoReserveWork();
+	// 모든 thread에서 공통으로 처리할 작업
 	static void DoGlobalWork();
 
 private:
+	// thread 별로 TLS 초기화 및 종료
 	static void InitTLS();
 	static void DestroyTLS();
 
