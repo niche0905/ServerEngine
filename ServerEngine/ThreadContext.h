f787a4d71253f8941f5266d24108fef109a67844
niche0905 <yudle537@gmail.com>
2025-11-24 21:22:41 +0900

    ThreadContext 추가
    TLS로 Thread 마다의 상태를 집합하여 둔 구조체를 생성하였다

---
+++ ServerEngine/ThreadContext.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "pch.h"
+
+/*-----------------
+   ThreadContext
+-----------------*/
+//
+// ThreadContext는 thread 별로 관리되는 컨텍스트입니다
+// TLS와 연동되어 각 thread가 자신의 Context(상태)를 가질 수 있도록 합니다
+//
+
+struct ThreadContext
+{
+	uint32 thread_id;		// thread 고유 id
+
+	// TODO: thread 별로 관리할 상태들 추가
+};
+
