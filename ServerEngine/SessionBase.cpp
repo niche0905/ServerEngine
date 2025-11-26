0783aa9d601789c6329137a93f973f3c7ddb6a26
niche0905 <yudle537@gmail.com>
2025-11-26 22:58:32 +0900

    SessionBase 작성
    Session이 나누어지기 전 Base Session을 담당하는 class를 만들었고 공통 함수들을 추상화 하였다
    추가로 고민해봐야 할 것은 SocketUtils을 통해서 Create Socket을 하는 부분과 PacketSession과 PacketHeader를 구성하는 부분이다

---
+++ ServerEngine/SessionBase.cpp
@@ -0,0 +1,70 @@
+#include "pch.h"
+#include "SessionBase.h"
+#include "NetworkAddress.h"
+
+/*---------------
+   SessionBase
+---------------*/
+
+SessionBase::SessionBase()
+{
+	socket_ = SocketUtils::CreateSocket();
+}
+
+SessionBase::~SessionBase()
+{
+	SocketUtils::Close(socket_);
+}
+
+void SessionBase::Disconnect(std::wstring_view cause)
+{
+	if (connected_.exchange(false) == false)
+		return;
+
+	// TODO: 로그 남기기(console logger 이용)
+
+	PostDisconnect();
+}
+
+void SessionBase::ProcessConnect()
+{
+	connected_.store(true);
+
+	// TODO: Service에 세션 등록
+
+	OnConnected();
+
+	// Recv waiting
+	PostRecv();
+}
+
+void SessionBase::ProcessDisconnect()
+{
+	OnDisconnected();
+	// TODO: Service에서 세션 해제
+}
+
+void SessionBase::HandleError(std::wstring_view functionName, int32 errorCode)
+{
+    std::wstring message = SocketUtils::GetWinErrorString(errorCode);
+
+    // TODO: 여기서 네 Logger 사용하면 됨
+    // 예: ConsoleLogger::Error(L"[Session] %s failed: (%d) %s",
+    //                           functionName.data(), errorCode, message.c_str());
+
+    // 임시 출력
+    wprintf(L"[Session] ERROR in %s (code: %d): %s\n",
+        functionName.data(), errorCode, message.c_str());
+
+    switch (errorCode)
+    {
+    case WSAECONNRESET:
+    case WSAECONNABORTED:
+    case WSAETIMEDOUT:
+        Disconnect(L"HandleError");
+        break;
+
+    default:
+        break;
+    }
+}
---
+++ ServerEngine/SessionBase.h
@@ -0,0 +1,75 @@
+#pragma once
+#include "IoObject.h"
+#include "NetworkAddress.h"
+
+/*---------------
+   SessionBase
+---------------*/
+//
+// SessionBase는 세션의 기본 클래스입니다
+//
+
+class SessionBase : public IoObject, public std::enable_shared_from_this<SessionBase>
+{
+	enum
+	{
+		BUFFER_SIZE = 0x1000,	// 4KB
+	};
+
+public:
+	SessionBase();
+	virtual ~SessionBase();
+
+// Architecture interface
+public:
+	// TODO: void Send()... 어떻게 분리해야 옳지?
+	virtual bool Connect(SOCKET socket) = 0;
+	void Disconnect(std::wstring_view cause);
+
+	// TODO: GetService()... Service부터 구현하고
+	//		 SetService()
+
+// Network interface
+public:
+	const NetAddr& GetNetAddr() const { return netAddr_; }
+	void SetNetAddr(const NetAddr& addr) { netAddr_ = addr; }
+
+	SOCKET GetSocket() { return socket_; }
+	bool IsConnected() const { return connected_; }
+
+// Event interface
+protected:
+	
+	// post is to request
+	virtual bool PostConnect() = 0;
+	virtual bool PostDisconnect() = 0;
+	virtual void PostRecv() = 0;
+	virtual void PostSend() = 0;
+
+	// process is to complete
+	void ProcessConnect();
+	void ProcessDisconnect();
+	virtual void ProcessRecv(int32 numOfBytes) = 0;
+	virtual void ProcessSend(int32 numOfBytes) = 0;
+
+
+// Error handling
+protected:
+	void HandleError(std::wstring_view functionName, int32 errorCode);
+
+// on event interface for content override
+protected:
+	virtual void OnConnected() {};
+	virtual void OnDisconnected() {};
+	virtual int32 OnRecv(BYTE* buffer, int32 len) { return len; }
+	virtual void OnSend(int32 len) {}
+
+protected:
+	// std::weakptr<ServiceBase>	service_{};
+	SOCKET				socket_{ INVALID_SOCKET };
+	NetAddr				netAddr_{};
+
+	std::atomic<bool>	connected_{ false };
+
+};
+
