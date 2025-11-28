48f70df95d64bb7a8939ea52ff8c8f365911f315
niche0905 <yudle537@gmail.com>
2025-11-28 20:27:34 +0900

    ServiceBase 구현
    Service가 가질 상태와 필요한 동작들을 생각해 보고 인터페이스를 구축해보았다

---
+++ ServerEngine/ServiceBase.cpp
@@ -0,0 +1,61 @@
+#include "pch.h"
+#include "ServiceBase.h"
+
+/*----------------
+   ServiceBase
+----------------*/
+
+ServiceBase::ServiceBase(ServiceType type, NetAddr address, SessionFactory factory, int32 maxSessionCount)
+	: type_(type)
+	, netAddress_(address)
+	, sessionFactory_(factory)
+	, maxSessionCount_(maxSessionCount)
+	, sessionCount_(0)
+{
+
+}
+
+ServiceBase::~ServiceBase()
+{
+
+}
+
+bool ServiceBase::CanStart() const
+{
+	return (nullptr != sessionFactory_);
+}
+
+void ServiceBase::StopService()
+{
+	
+}
+
+std::shared_ptr<SessionBase> ServiceBase::CreateSession()
+{
+	std::shared_ptr<SessionBase> session = sessionFactory_();
+	// TODO: Interface 설정 후 호출하기
+	//session->SetService(shared_from_this());
+	bool registerSuccess = RegisterSession(session);
+	if (not registerSuccess) {
+		return nullptr;
+	}
+
+	return session;
+}
+
+void ServiceBase::AddSession(std::shared_ptr<SessionBase> session)
+{
+	// TODO: lock free로 효율적인 구현이 가능하다면 변경하도록
+	std::lock_guard<std::mutex> lock(sessionMutex_);
+
+	++sessionCount_;
+	sessions_.insert(session);
+}
+
+void ServiceBase::RemoveSession(std::shared_ptr<SessionBase> session)
+{
+	std::lock_guard<std::mutex> lock(sessionMutex_);
+
+	assert(sessions_.erase(session) != 0);
+	--sessionCount_;
+}
---
+++ ServerEngine/ServiceBase.h
@@ -0,0 +1,57 @@
+#pragma once
+#include "IoBackend.h"
+#include "SessionBase.h"
+
+/*----------------
+   ServiceBase
+----------------*/
+//
+// ServiceBase는 Service의 기본 인터페이스입니다
+// 다양한 종류의 Service가 이 클래스를 상속받아 구현됩니다
+//
+
+using SessionFactory = std::function<std::shared_ptr<SessionBase>(void)>;
+
+class ServiceBase : public std::enable_shared_from_this<ServiceBase>
+{
+public:
+	ServiceBase() = delete;
+	ServiceBase(ServiceType type, NetAddr address, SessionFactory factory, int32 maxSessionCount = 1);
+	virtual ~ServiceBase();
+
+	// Service Start
+	virtual bool Start() = 0;
+	virtual bool CanStart() const;
+
+	virtual void StopService();
+
+	// Session Factory Setting
+	void SetSessionFactory(SessionFactory factory) { sessionFactory_ = factory; }
+
+	// Session Management
+	std::shared_ptr<SessionBase> CreateSession();
+	virtual bool RegisterSession(std::shared_ptr<SessionBase> session) = 0;
+	void AddSession(std::shared_ptr<SessionBase> session);
+	void RemoveSession(std::shared_ptr<SessionBase> session);
+	int32 GetCurrentSessionCount() const { return sessionCount_; }
+	int32 GetMaxSessionCount() const { return maxSessionCount_; }
+
+// Service Information
+public:
+	ServiceType GetServiceType() const { return type_; }
+	NetAddr GetNetAddress() const { return netAddress_; }
+	virtual void DoWork() = 0;
+
+protected:
+	ServiceType type_;									// what kind of service
+	NetAddr netAddress_;								// network address info
+
+	SessionFactory sessionFactory_;						// session factory function
+	int32 maxSessionCount_;								// maximum session count
+	std::atomic<int32> sessionCount_;					// current session count
+	std::set<std::shared_ptr<SessionBase>> sessions_;	// active sessions
+
+	std::mutex sessionMutex_;							// mutex for session management
+
+};
+
