b31f9d7a24456e4976510b0b56681ec81e5978a8
Niche <73772820+niche0905@users.noreply.github.com>
2025-12-30 16:24:00 +0900

    PlayerSession 초안 작성
    PlayerSession 코드의 초안을 작성하였다

---
+++ TimeThiefServer/Network/Session/PlayerSession.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include "Network/Session/IOCP/IocpSession.h"
+
+/*-----------------
+   PlayerSession
+-----------------*/
+//
+// PlayerSession은 게임 플레이어와의 네트워크 세션을 관리합니다.
+//
+
+// TODO: Temp로 IocpSession 상속, 추후 필요에 따라 기능 추가 예정
+class PlayerSession : public IocpSession
+{
+public:
+   PlayerSession();
+   virtual ~PlayerSession();
+   
+private:
+   // TODO: 플레이어 세션 관련 멤버 변수 추가 예정
+    
+};
