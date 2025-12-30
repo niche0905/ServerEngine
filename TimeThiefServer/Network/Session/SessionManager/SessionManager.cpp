f691fc9e49df9cf8b560b82425af0de2d4daec10
Niche <73772820+niche0905@users.noreply.github.com>
2025-12-30 20:09:53 +0900

    SessionManager 구현
    간단하게 SessionManager를 lock 기반으로 구현하였다

---
+++ TimeThiefServer/Network/Session/SessionManager/SessionManager.cpp
@@ -0,0 +1,40 @@
+﻿#include "pch.h"
+#include "SessionManager.h"
+
+/*------------------
+   SessionManager
+------------------*/
+
+SessionManager g_SessionManager;
+
+void SessionManager::Add(SessionRef session)
+{
+   std::lock_guard<std::mutex> lock(mutex_);
+   sessions_.insert(session);
+}
+
+void SessionManager::Remove(SessionRef session)
+{
+   std::lock_guard<std::mutex> lock(mutex_);
+   sessions_.erase(session);
+}
+
+void SessionManager::Clear()
+{
+   std::lock_guard<std::mutex> lock(mutex_);
+   sessions_.clear();
+}
+
+void SessionManager::Broadcast(SendBufferRef sendBuffer)
+{
+   std::lock_guard<std::mutex> lock(mutex_);
+   
+   for (const auto& session : sessions_) {
+      session->Send(sendBuffer);
+   }
+}
+
+size_t SessionManager::GetSessionCount() const
+{
+   return sessions_.size();
+}
---
+++ TimeThiefServer/Network/Session/SessionManager/SessionManager.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+#include "Network/Session/PlayerSession.h"
+#include "Network/Buffer/SendBuffer.h"
+
+/*------------------
+   SessionManager
+------------------*/
+//
+// SessionManager는 모든 네트워크 세션을 관리합니다.
+// 컨텐츠(게임 서버) 코드에서 사용하기 위한 세션 관리 기능을 제공합니다.
+//
+
+class PlayerSession;
+class SendBuffer;
+
+class SessionManager
+{
+private:
+   using SessionRef = std::shared_ptr<PlayerSession>;
+   using SendBufferRef = std::shared_ptr<SendBuffer>;
+   
+public:
+   void Add(SessionRef session);
+   void Remove(SessionRef session);
+   
+   void Clear();
+   
+   void Broadcast(SendBufferRef sendBuffer);
+   size_t GetSessionCount() const;
+   
+private:
+   std::mutex mutex_;   // 세션 리스트 보호용 뮤텍스
+   std::unordered_set<SessionRef> sessions_; // 활성 세션 리스트
+    
+};
+
+extern SessionManager g_SessionManager;
