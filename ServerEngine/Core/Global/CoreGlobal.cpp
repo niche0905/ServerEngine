74a9f2023813cd07e5cefa265a1742517a1c422e
Niche <73772820+niche0905@users.noreply.github.com>
2026-01-02 17:17:57 +0900

    CoreGlobal 추가
    Engine에서 사용할 전역 변수들을 보다 편하게 이용하기 위해서 이를 추가하였다

---
+++ ServerEngine/Core/Global/CoreGlobal.cpp
@@ -0,0 +1,37 @@
+﻿#include "pch.h"
+#include "CoreGlobal.h"
+#include "Utils/Log/ConsoleLogger.h"
+
+/*--------------
+   CoreGlobal
+--------------*/
+//
+// CoreGlobal는 글로벌 설정 및 상태를 관리하는 클래스입니다
+// 이 클래스는 애플리케이션 전반에 걸쳐 사용되는 전역 변수와 설정을 포함합니다
+//
+
+ConsoleLogger* consoleLogger = nullptr;
+
+class CoreGlobal
+{
+public:
+    CoreGlobal()
+    {
+        std::wcout << L"CoreGlobal Initial" << std::endl;
+        
+        consoleLogger = new ConsoleLogger();      // 콘솔 로거 초기화(생성)
+      
+        SocketUtils::Initialize();                // 소켓 유틸리티 초기화
+    }
+   
+    ~CoreGlobal()
+    {
+        std::wcout << L"CoreGlobal Destruct" << std::endl;
+
+        delete consoleLogger;                     // 콘솔 로거 해제(삭제)
+        consoleLogger = nullptr;
+        
+        SocketUtils::Clean();                   // 소켓 유틸리티 정리(종료)
+    }
+    
+} coreGlobalInstance;  // 전역 인스턴스 생성
