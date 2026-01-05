c99335d1ca686115cce41ca7b87da1f8144c5d74
Niche <73772820+niche0905@users.noreply.github.com>
2026-01-05 22:39:52 +0900

    IocpEvent::ResetOverlapped 변경
    이게 유효한 변경인진 모른다 (차이가 없을 수도 있다)

--- ServerEngine/Network/Event/IOCP/IocpEvent.h
+++ ServerEngine/Network/Event/IOCP/IocpEvent.h
@@ -16,7 +16,13 @@ public:
 
 	void ResetOverlapped() noexcept
 	{
-		ZeroMemory(static_cast<OVERLAPPED*>(this), sizeof(OVERLAPPED));
+		// ZeroMemory(static_cast<OVERLAPPED*>(this), sizeof(OVERLAPPED));
+		
+		OVERLAPPED::hEvent = 0;
+		OVERLAPPED::Internal = 0;
+		OVERLAPPED::InternalHigh = 0;
+		OVERLAPPED::Offset = 0;
+		OVERLAPPED::OffsetHigh = 0;
 	}
 
 	virtual IoEventType GetType() const noexcept override { return type_; }
