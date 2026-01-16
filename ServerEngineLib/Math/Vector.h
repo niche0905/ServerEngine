86c4418a8e8f9d1e8564bf68589f358a36e98d95
niche0905 <yudle537@gmail.com>
2026-01-16 17:40:03 +0900

    Vector3 추가
    Vector3를 일단 간단하게 작성하여 구성했다

---
+++ ServerEngineLib/Math/Vector.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+/*-----------
+   Vector3
+-----------*/
+//
+// Vector3는 Server Engine 내/외부에서 사용할 가능성이 있는 좌표 표현 Vector입니다
+//
+
+namespace SE::Math
+{
+    struct Vector3
+    {
+        float x, y, z;
+
+        constexpr Vector3() : x(0), y(0), z(0) {}
+        constexpr Vector3(float x, float y, float z)
+            : x(x), y(y), z(z) {}
+
+        Vector3 operator+(const Vector3& rhs) const
+        {
+            return { x + rhs.x, y + rhs.y, z + rhs.z };
+        }
+
+        Vector3 operator-(const Vector3& rhs) const
+        {
+            return { x - rhs.x, y - rhs.y, z - rhs.z };
+        }
+
+        Vector3 operator*(float s) const
+        {
+            return { x * s, y * s, z * s };
+        }
+    };
+}
