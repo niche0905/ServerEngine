eb4f786d4c3213470e993b12256b11364d9b6c6a
niche0905 <yudle537@gmail.com>
2026-01-16 17:40:45 +0900

    CollsionResult 작성
    기본적으로 필요할 것이라 생각되는 정보를 구조체로 묶어서 구현하였다

---
+++ ServerEngineLib/Physics/Collider/CollsionResult.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+#include "Math/Vector.h"
+
+/*-------------------
+   CollisionResult
+-------------------*/
+//
+// CollisionResult는 충돌 검사 후 반환해야 할 정보를 담을 구조체입니다
+//
+
+namespace SE::Physics
+{
+    using Math::Vector3;
+    
+    struct CollisionResult
+    {
+        bool hit = false;
+        Vector3 point;
+        Vector3 normal;
+        float penetration;
+    };
+    
+}
