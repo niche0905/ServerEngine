e36f86e5c390dbcc7ad582304c21661e54b9d324
niche0905 <yudle537@gmail.com>
2026-01-16 17:41:20 +0900

    Collider 작성
    Collider의 기본 인터페이스를 작성하였다
    이를 상속받아 4개의 Collider를 우선적으로 만들어 볼 생각이다

---
+++ ServerEngineLib/Physics/Collider/Collider.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+#include "CollsionResult.h"
+
+/*------------
+   Collider
+------------*/
+//
+// Collider는 서버 컨텐츠에서 사용하기 위해 만든 충돌체 클래스 입니다
+//
+
+namespace SE::Physics
+{
+    struct CollisionResult;
+    
+    enum class ColliderType : uint8
+    {
+        AABB,
+        OBB,
+        Sphere,
+        Capsule,
+    };
+    
+    class Collider
+    {
+    public:
+        virtual ~Collider() = default;
+        
+        virtual ColliderType GetType() const = 0;
+        virtual bool Intersect(const Collider& other, CollisionResult& out) const = 0;
+    };
+    
+}
