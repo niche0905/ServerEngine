4e40e7344d391ba17f96b48ab46a86b498ce46d1
niche0905 <yudle537@gmail.com>
2026-01-19 15:40:42 +0900

    Sphere Collider 구현
    Sphere Collider를 완성하였다

---
+++ ServerEngineLib/Physics/Collider/SphereCollider.cpp
@@ -0,0 +1,65 @@
+﻿#include "pch.h"
+#include "SphereCollider.h"
+
+/*------------------
+   SphereCollider
+------------------*/
+
+namespace SE::Physics
+{
+   static inline float AbsF(float v) { return (v < 0.0f) ? -v : v; }
+   
+   //-----------------------------------------------------------------------------
+   //-----------------------------------------------------------------------------
+   
+   SphereCollider::SphereCollider(const Vector3& center, float radius)
+   {
+      Set(center, radius);
+   }
+
+   ColliderType SphereCollider::GetType() const
+   {
+      return ColliderType::Sphere;
+   }
+
+   Collider* SphereCollider::Clone() const
+   {
+      return new SphereCollider(*this);
+   }
+
+   bool SphereCollider::Intersect(const Collider& other, CollisionResult& out) const
+   {
+      // TODO: 충돌체 충돌 판정 방식 우선 개발
+      
+      return false;
+   }
+
+   void SphereCollider::Set(const Vector3& center, float radius)
+   {
+      center_ = center;
+      radius_ = AbsF(radius);
+      
+      RecaleWorldAABB();
+   }
+
+   const AABBCollider& SphereCollider::GetWorldAABB() const
+   {
+      return worldAABB_;
+   }
+
+   bool SphereCollider::Contains(const Vector3& point) const
+   {
+      const float dist = (center_ - point).LengthSq();
+      return dist <= RadiusSq();
+   }
+
+   void SphereCollider::RecaleWorldAABB()
+   {
+      const Vector3 r{radius_, radius_, radius_};
+      
+      const Vector3 mn = center_ - r;
+      const Vector3 mx = center_ + r;
+      
+      worldAABB_.SetMinMax(mn, mx);
+   }
+}
---
+++ ServerEngineLib/Physics/Collider/SphereCollider.h
@@ -0,0 +1,54 @@
+﻿#pragma once
+#include "AABBCollider.h"
+#include "Collider.h"
+#include "Math/Vector.h"
+
+/*------------------
+   SphereCollider
+------------------*/
+//
+// SphereCollider는 구체 형태의 충돌체입니다
+//
+
+namespace SE::Physics
+{
+    class SphereCollider : public Collider
+    {
+    public:
+        using Vector3 = SE::Math::Vector3;
+        
+    public:
+        SphereCollider() = default;
+        virtual ~SphereCollider() = default;
+        
+        SphereCollider(const Vector3& center, float radius);
+        
+        virtual ColliderType GetType() const override;
+        virtual Collider* Clone() const;
+        
+        virtual bool Intersect(const Collider& other, CollisionResult& out) const override;
+        
+    public:
+        void Set(const Vector3& center, float radius);
+        
+        const Vector3& GetCenter() const { return center_; }
+        float GetRadius() const { return radius_; }
+        
+        const AABBCollider& GetWorldAABB() const;
+        
+        bool Contains(const Vector3& point) const;
+        float RadiusSq() const { return radius_ * radius_; }
+        
+    private:
+        void RecaleWorldAABB();
+        
+    private:
+        Vector3 center_;
+        float radius_;
+        
+        AABBCollider worldAABB_;
+    
+    };
+    
+}
+
