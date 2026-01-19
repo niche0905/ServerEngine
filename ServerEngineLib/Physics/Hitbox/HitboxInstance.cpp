8577d52855944236763343b83da7db0cbf9e71cb
niche0905 <yudle537@gmail.com>
2026-01-19 22:52:27 +0900

    HitboxInstance 초안 작성
    HitboxInstance로 실제 게임 객체가 가지게 될 Hitbox를 관리하고 충돌 처리를 판정하는 class의 초기 구조를 설계하고 작성하였다
    우선적으로 Ray에 관련한 개념과 코드를 작성하고 필요한 것들을 추가해야 할 것으로 보인다

---
+++ ServerEngineLib/Physics/Hitbox/HitboxInstance.cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+#include "HitboxInstance.h"
+
+/*------------------
+   HitboxInstance
+------------------*/
+
+namespace SE::Physics::Hit
+{
+   void HitboxInstance::Bind(const HitboxAsset* asset)
+   {
+      asset_ = asset;
+   }
+
+   void HitboxInstance::Update(const Vector3& rootPos, float yawRadians)
+   {
+      // TODO: 채워라
+   }
+
+   const AABBCollider& HitboxInstance::GetWorldAABB() const
+   {
+      return worldAABB_;
+   }
+}
---
+++ ServerEngineLib/Physics/Hitbox/HitboxInstance.h
@@ -0,0 +1,52 @@
+﻿#pragma once
+#include "HitboxPart.h"
+#include "Physics/Collider/AABBCollider.h"
+
+/*------------------
+   HitboxInstance
+------------------*/
+//
+// HitboxInstance는 한 객체(Hitbox를 가져야 할) Hitbox 정보를 가지게 될 클래스입니다
+//
+
+namespace SE::Physics::Hit
+{
+    class HitboxAsset
+    {
+    public:
+        std::vector<HitboxPart> parts;
+        
+    };
+    
+    class HitboxInstance
+    {
+    public:
+        using Vector3 = SE::Math::Vector3;
+        
+    public:
+        void Bind(const HitboxAsset* asset);
+        void Update(const Vector3& rootPos, float yawRadians);
+        
+        const AABBCollider& GetWorldAABB() const;
+        
+        // TODO: Ray 먼저 작성하기 SE::Physics의 영역이다
+        bool Raycast(class Ray& ray, class RaycastHit& out) const;
+
+    private:
+        const HitboxAsset* asset_ = nullptr;
+        
+        // TODO: 런타임 캐시가 필요하게 된다면 사용할 것
+        struct WorldPart
+        {
+            HitShapeType type;
+            HitGroup group;
+            float mult;
+            
+        };
+        
+        std::vector<WorldPart> worldParts_;
+        AABBCollider worldAABB_;
+    
+    };
+    
+}
