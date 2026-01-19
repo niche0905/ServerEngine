a5c09f21b1658ed80a6c1a583a4b963315f83234
niche0905 <yudle537@gmail.com>
2026-01-19 22:38:07 +0900

    HitboxPart 추가
    HitboxPart로 피격 가능한 분리된 부위의 정보를 담고있는 구조체를 정의하였다

---
+++ ServerEngineLib/Physics/Hitbox/HitboxPart.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+#include "HitGroup.h"
+
+/*--------------
+   HitboxPart
+--------------*/
+//
+// HitboxPart는 피격 가능한 한 부위의 정보 집합입니다
+//
+
+namespace SE::Physics::Hit
+{
+    enum class HitShapeType : uint8
+    {
+        Sphere,
+        Capsule,
+        OBB,
+    };
+    
+    struct HitboxPart
+    {
+        HitShapeType type;
+        HitGroup group;
+        float damageMultiplier;
+        
+        SE::Math::Vector3 localOffset;  // root에서의 중심/기준점 으로부터
+        float radius;
+    };
+    
+}
