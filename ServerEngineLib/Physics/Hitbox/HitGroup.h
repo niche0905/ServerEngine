7725a34d5d7e5896be367e03d9101442900efb77
niche0905 <yudle537@gmail.com>
2026-01-19 22:32:04 +0900

    HitGroup 정의
    HItbox 개념의 도입을 위해
    SE::Physics::Hit에 HitGroup이라는 enum으로 Type을 정의하였다

---
+++ ServerEngineLib/Physics/Hitbox/HitGroup.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+/*------------
+   HitGroup
+------------*/
+//
+// HitGroup은 슈팅에 피격될 수 있는 HitboxPart의 Type입니다
+//
+
+namespace SE::Physics::Hit
+{
+    enum class HitGroup : uint8
+    {
+        Head,   // 머리
+        Torso,  // 몸통
+        Arms,   // 팔
+        Legs,   // 다리
+    };
+   
+}
