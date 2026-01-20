6c2f8a6ec05efb6eb0a42043220c8bfefc4335aa
niche0905 <yudle537@gmail.com>
2026-01-20 21:02:15 +0900

    Compound Collider 추가
    복합 충돌체를 표현하기 위한 class를 설계하였고
    아무래도 건물과 같은 객체를 표현하기 위해 사용될 것으로 예상된다

---
+++ ServerEngineLib/Physics/Collider/CompoundCollider.cpp
@@ -0,0 +1,100 @@
+﻿#include "pch.h"
+#include "CompoundCollider.h"
+
+/*--------------------
+   CompoundCollider
+--------------------*/
+
+namespace SE::Physics
+{
+   ColliderType CompoundCollider::GetType() const
+   {
+      return ColliderType::Compound;
+   }
+
+   Collider* CompoundCollider::Clone() const
+   {
+      auto* c = new CompoundCollider();
+      c->Reserve(colliders_.size());
+      
+      for (auto& child : colliders_)
+         c->Add(std::unique_ptr<Collider>(child->Clone()));
+      
+      c->dirtyAABB_ = true;
+      c->RecalcWorldAABB();
+      
+      return c;
+   }
+
+   bool CompoundCollider::Intersect(const Collider& other, CollisionResult& out) const
+   {
+      // TODO: 나중에
+      return false;
+   }
+
+   void CompoundCollider::Reserve(size_t n)
+   {
+      colliders_.reserve(n);
+   }
+
+   void CompoundCollider::Clear()
+   {
+      colliders_.clear();
+      dirtyAABB_ = true;
+   }
+
+   void CompoundCollider::Add(ColliderRef collider)
+   {
+      if (!collider) {
+         // TODO: 디버그 일 때만 아래를 실행하도록 설정
+         {
+            assert(false && "CompoundCollider::Add - collider is nullptr");
+         }
+         return;
+      }
+      colliders_.push_back(std::move(collider));
+      dirtyAABB_ = true;
+   }
+
+   const AABBCollider& CompoundCollider::GetWorldAABB() const
+   {
+      if (dirtyAABB_)
+         RecalcWorldAABB();
+      
+      return worldAABB_;
+   }
+
+   void CompoundCollider::RecalcWorldAABB() const
+   {
+      dirtyAABB_ = false;
+      
+      if (colliders_.empty()) {
+         worldAABB_.SetMinMax(Vector3(0, 0, 0), Vector3(0, 0, 0));
+         return;
+      }
+      
+      // Initialize with the first collider's AABB
+      const AABBCollider& first = colliders_[0]->GetWorldAABB();
+      Vector3 mn = first.GetMin();
+      Vector3 mx = first.GetMax();
+      
+      // Expand to include all other colliders' AABBs
+      for (size_t i = 1; i < colliders_.size(); ++i) {
+         
+         const AABBCollider& aabb = colliders_[i]->GetWorldAABB();
+         
+         const Vector3& cmn = aabb.GetMin();
+         const Vector3& cmx = aabb.GetMax();
+         
+         mn.x =SE::Math::Min(mn.x, cmx.x);
+         mn.y =SE::Math::Min(mn.y, cmx.y);
+         mn.z =SE::Math::Min(mn.z, cmx.z);
+         
+         mx.x =SE::Math::Max(mx.x, cmx.x);
+         mx.y =SE::Math::Max(mx.y, cmx.y);
+         mx.z =SE::Math::Max(mx.z, cmx.z);
+      }
+      
+      worldAABB_.SetMinMax(mn, mx);
+   }
+}
---
+++ ServerEngineLib/Physics/Collider/CompoundCollider.h
@@ -0,0 +1,59 @@
+﻿#pragma once
+#include "Collider.h"
+#include "AABBCollider.h"
+
+/*--------------------
+   CompoundCollider
+--------------------*/
+//
+// CompoundCollider는 복합적인 프리미티브를 가져야 할 필요성이 있는 충돌체입니다
+//
+
+namespace SE::Physics
+{
+    class CompoundCollider : public Collider
+    {
+    public:
+        using Vector3 = SE::Math::Vector3;
+        using ColliderRef = std::unique_ptr<Collider>;
+        
+    public:
+        CompoundCollider() = default;
+        virtual ~CompoundCollider() = default;
+        
+        virtual ColliderType GetType() const override;
+        virtual Collider* Clone() const override;
+        
+        virtual bool Intersect(const Collider& other, CollisionResult& out) const override;
+        
+    public:
+        void Reserve(size_t n);
+        void Clear();
+        
+        void Add(ColliderRef collider);
+        
+        template <typename T, typename... Args>
+        T& Emplace(Args&&... args)
+        {
+            auto ptr = std::make_unique<T>(std::forward<Args>(args)...);
+            T& ref = *ptr;
+            colliders_.push_back(std::move(ptr));
+            dirtyAABB_ = true;
+            return ref;
+        }
+        
+        const AABBCollider& GetWorldAABB() const override;
+        
+    private:
+        void RecalcWorldAABB() const;
+        
+    private:
+        std::vector<ColliderRef> colliders_;
+        
+        mutable bool dirtyAABB_{true};
+        mutable AABBCollider worldAABB_;
+    
+    };
+   
+}
+
