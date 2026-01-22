e7b8f988ffde502f515cd5c5f5843791dee4ea48
niche0905 <yudle537@gmail.com>
2026-01-22 18:33:56 +0900

    SphereCollider::Raycast 구현
    간단한 구체 충돌체의 Raycast부터 구현해 보았다

--- ServerEngineLib/Physics/Collider/SphereCollider.cpp
+++ ServerEngineLib/Physics/Collider/SphereCollider.cpp
@@ -1,5 +1,7 @@
 ﻿#include "pch.h"
 #include "SphereCollider.h"
+#include "Physics/Ray/Ray.h"
+#include "Physics/Ray/RaycastHit.h"
 
 /*------------------
    SphereCollider
@@ -49,7 +51,39 @@ namespace SE::Physics
 
    bool SphereCollider::Raycast(const Ray& ray, RaycastHit& out) const
    {
-      return false;
+      // TODO: 디버그 일 때만 아래를 실행하도록 설정
+      {
+         assert(SE::Math::NearlyZero(ray.direction.LengthSq() - 1.0f, 1e-3f) && "Ray direction must be normalized");
+      }
+      
+      const Vector3 m = ray.origin - center_;
+      
+      const float halfB = m.Dot(ray.direction);
+      const float c = m.Dot(m) - (radius_ * radius_);
+      
+      const float disc = halfB * halfB - c;
+      if (disc < 0.0f)  // 교차 없음
+         return false;
+      
+      const float sqrtDisc = std::sqrt(disc);
+      
+      float t = -halfB - sqrtDisc;
+      if (t < ray.tMin or t > ray.tMax) {
+         t = -halfB + sqrtDisc;
+         if (t < ray.tMin or t > ray.tMax) {
+            return false;  // 교차 없음 (유효하지 않은 교차점)
+         }
+      }
+      
+      out.hit = true;
+      out.t = t;
+      out.point = ray.At(t);
+      
+      out.normal = (out.point - center_).Normalized(Vector3(0.0f, 1.0f, 0.0f));     // 노멀 단위벡터
+                                                                                          // 만약 중심과 일치하면 Y축 방향으로 설정(임의 설정)
+      out.collider = this;
+      
+      return true;
    }
 
    bool SphereCollider::Contains(const Vector3& point) const
