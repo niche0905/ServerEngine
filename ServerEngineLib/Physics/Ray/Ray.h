57ad84de2b79c5b3fb3e2e5b3ebc719a58c072ea
niche0905 <yudle537@gmail.com>
2026-01-22 17:37:09 +0900

    Ray 추가
    Ray라는 간단한 구조체를 추가하였다
    Raycast 시 사용될 Ray 객체를 담당한다

---
+++ ServerEngineLib/Physics/Ray/Ray.h
@@ -0,0 +1,44 @@
+﻿#pragma once
+
+/*-------
+   Ray
+-------*/
+//
+// Ray는 광선을 나타내는 구조체입니다
+// Raycast로 충돌을 검사할 때 사용됩니다
+//
+
+namespace SE::Physics
+{
+    struct Ray
+    {
+    public:
+        using Vector3 = SE::Math::Vector3;
+        
+    public:
+        Vector3 origin{};    // 광선의 시작점
+        Vector3 direction{}; // 광선의 방향(단위 벡터)
+        
+        float tMin = 0.0f;   // 시작 파라미터
+        float tMax = 1e30f;  // 끝 파라미터 (사거리)
+        
+        Ray() = default;
+        
+        Ray(const Vector3& o, const Vector3& d, float maxDist)
+            : origin(o), direction(d), tMin(0.0f), tMax(maxDist)
+        {
+        }
+        
+        Ray(const Vector3& o, const Vector3& d, float minDist, float maxDist)
+            : origin(o), direction(d), tMin(minDist), tMax(maxDist)
+        {
+        }
+        
+        // t 위치의 실제 월드 좌표
+        Vector3 At(float t) const
+        {
+            return origin + direction * t;
+        }
+    };
+    
+}
