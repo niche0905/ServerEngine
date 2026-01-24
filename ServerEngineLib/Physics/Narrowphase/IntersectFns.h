0a03a97d75b44534ea862dbc7771683f19762364
niche0905 <yudle537@gmail.com>
2026-01-24 22:17:38 +0900

    IntersectFns 정의
    IntersectFns.h의 정으로 구현할 충돌체 끼리의 충돌 판정 함수의 인터페이스를 모아두기로 하였다

---
+++ ServerEngineLib/Physics/Narrowphase/IntersectFns.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include "Physics/Collider/Collider.h"
+
+/*----------------
+   IntersectFns
+----------------*/
+//
+// IntersectFns 헤더는 실제 구현될 충돌 판정 함수들의 선언을 포함합니다
+//
+
+namespace SE::Physics::Narrowphase
+{
+   
+   bool Intersect_AABB_AABB(const Collider& a, const Collider& b, CollisionResult& out);
+   
+   
+}
