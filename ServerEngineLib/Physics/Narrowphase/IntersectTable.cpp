01860f9e49251b69b31d61fb801be8d6faa11979
niche0905 <yudle537@gmail.com>
2026-01-24 22:02:41 +0900

    IntersectTable 정의
    namespace로 실제 충돌 구현이 담길 Narrowphase에 LUT인 IntersectTable을 정의하게 되었다

---
+++ ServerEngineLib/Physics/Narrowphase/IntersectTable.cpp
@@ -0,0 +1,41 @@
+﻿#include "pch.h"
+#include "IntersectTable.h"
+
+/*---------------
+   Narrowphase
+---------------*/
+
+namespace SE::Physics::Narrowphase
+{
+   constexpr int kTypeCount = static_cast<int>(ColliderType::Compound) + 1;
+   
+   static IntersectFn gTable[kTypeCount][kTypeCount];
+   
+   // 구현이 없는 충돌 판정 함수
+   static bool NotSupported(const Collider&, const Collider&, CollisionResult&)
+   {
+      return false;
+   }
+   
+   void InitIntersectTable()
+   {
+      // 모든 조합을 NotSupported로 초기화
+      for (int i = 0; i < kTypeCount; ++i) {
+         for (int j = 0; j < kTypeCount; ++j) {
+            gTable[i][j] = NotSupported;
+         }
+      }
+      
+      // 지원하는 충돌 판정 함수 등록
+      // 예: gTable[static_cast<int>(ColliderType::AABB)][static_cast<int>(ColliderType::OBB)] = AABBvsOBB;
+      // 실제 구현된 함수로 교체 필요
+      
+      // TODO: 충돌 판정 함수 구현 후 등록
+   }
+   
+   IntersectFn GetIntersectFn(ColliderType a, ColliderType b)
+   {
+      return gTable[static_cast<int>(a)][static_cast<int>(b)];
+   }
+   
+}
---
+++ ServerEngineLib/Physics/Narrowphase/IntersectTable.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include "Physics/Collider/Collider.h"
+
+/*---------------
+   Narrowphase
+---------------*/
+//
+// Narrowphase 네임스페이스는 충돌 판정의 세부 구현을 담당합니다
+//
+
+namespace SE::Physics::Narrowphase
+{
+    // IntersectFn는 실제 충돌 판정을 수행하는 함수 포인터 타입입니다
+    using IntersectFn = bool(*)(const Collider&, const Collider&, CollisionResult&);
+    
+    // LUT 초기화 (엔진 시작 시 1회 초기화)
+    void InitIntersectTable();
+    
+    // LUT 접근자
+    IntersectFn GetIntersectFn(ColliderType a, ColliderType b);
+}
