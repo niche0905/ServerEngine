f628d4d6b720c3be89d5c17730f301766e9634e8
niche0905 <yudle537@gmail.com>
2026-01-24 22:06:46 +0900

    IntersectUtil 작성
    SwapWrapper로 구현의 양을 반으로 줄이기 위해서 사용하는 Wrapper 함수이다

---
+++ ServerEngineLib/Physics/Narrowphase/IntersectUtil.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "IntersectUtil.h"
+#include "Physics/Collider/CollisionResult.h"
+
+/*---------------
+   SwapWrapper
+---------------*/
+
+namespace SE::Physics::Narrowphase
+{
+   bool SwapWrapper(const Collider& a, const Collider& b, CollisionResult& out, IntersectFn fn)
+   {
+      CollisionResult tmp;
+      if (not fn(b, a, tmp))
+         return false;
+      
+      tmp.normal = tmp.normal;
+      out = tmp;
+      return true;
+   }
+}
---
+++ ServerEngineLib/Physics/Narrowphase/IntersectUtil.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include "Physics/Collider/Collider.h"
+
+/*---------------
+   SwapWrapper
+---------------*/
+//
+// SwapWrapper는 충돌 판정 함수에서 콜라이더의 순서를 바꿔 호출하는 유틸리티 함수입니다
+// 구현의 편의를 위해 사용됩니다
+//
+
+namespace SE::Physics::Narrowphase
+{
+    using IntersectFn = bool(*)(const Collider&, const Collider&, CollisionResult&);
+    
+    bool SwapWrapper(const Collider& a, const Collider& b, CollisionResult& out, IntersectFn fn);
+    
+}
