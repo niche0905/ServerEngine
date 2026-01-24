16e7b0d3df125ec0588b0b0b307db2aa5f181179
niche0905 <yudle537@gmail.com>
2026-01-24 23:27:46 +0900

    CompoundCollider::intersect 처리
    Collider에서 Compound의 경우 LUT에 없으므로 직접 처리하도록 하는 코드를 집어 넣었다

--- ServerEngineLib/Physics/Collider/Collider.cpp
+++ ServerEngineLib/Physics/Collider/Collider.cpp
@@ -1,5 +1,6 @@
 ﻿#include "pch.h"
 #include "Collider.h"
+#include "CollisionResult.h"
 #include "Physics/Narrowphase/IntersectTable.h"
 
 /*------------
@@ -11,6 +12,17 @@ namespace SE::Physics
 {
    bool Collider::Intersect(const Collider& other, CollisionResult& out) const
    {
+      if (other.GetType() == ColliderType::Compound) {
+         // Compound vs Other 충돌 판정은 반대 순서로 처리
+         CollisionResult tempResult;
+         if (not other.Intersect(*this, tempResult))
+            return false;
+         
+         tempResult.normal = -tempResult.normal;
+         out = tempResult;
+         return true;
+      }
+      
       using namespace SE::Physics::Narrowphase;
       
       const auto fn = GetIntersectFn(GetType(), other.GetType());
