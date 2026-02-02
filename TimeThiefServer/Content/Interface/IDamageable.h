dd094254ca10cdf171f3c8445c257bbdf7d05dd8
niche0905 <yudle537@gmail.com>
2026-02-03 00:58:25 +0900

    IDamageable 인터페이스 작성
    IDamageable이란 인터페이스로 데미지를 받을 수 있게하는 인터페이스를 추가하였다

---
+++ TimeThiefServer/Content/Interface/IDamageable.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+#include "Content/Enum/DamageTypes.h"
+
+class ObjectManager;
+
+/*----------------
+   IDamageable
+----------------*/
+//
+// IDamageable은 피해를 입을 수 있는 객체가 구현해야 하는 인터페이스입니다.
+// HealthComponent와 같은 컴포넌트를 이용하도록 설계되었습니다.
+//
+
+class IDamageable
+{
+public:
+   virtual ~IDamageable() = default;
+   
+   virtual DamageResult ApplyDamage(ObjectManager& om, int32 amount, const DamageContext& ctx) = 0;
+   
+   virtual bool IsAlive() const = 0;
+   virtual int32 GetHp() const = 0;
+   virtual int32 GetMaxHp() const = 0;
+   
+};
