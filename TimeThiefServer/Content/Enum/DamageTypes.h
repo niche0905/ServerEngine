70bc18d7b1616ecf0ef40140032da3524aae259e
niche0905 <yudle537@gmail.com>
2026-02-03 00:57:50 +0900

    DamageType 정의
    DamageType 및 DamageSource의 enum 값을 정의하고 DamageContext, DamageResult를 옮겨왔다

---
+++ TimeThiefServer/Content/Enum/DamageTypes.h
@@ -0,0 +1,44 @@
+﻿#pragma once
+
+struct ObjectId;
+
+enum class DamageType : int8
+{
+    None = 0,
+    
+    Melee,
+    Ranged,
+    Explosion,
+    Dot,
+    TrueDamage,
+};
+
+enum class DamageSource : int8
+{
+    Unknown = 0,
+    
+    Skill,
+    Weapon,
+    Environment,
+};
+
+struct DamageContext
+{
+    ObjectId attacker{};    // 공격자
+    ObjectId instigator{};  // 발사체 등의 주인
+    int32 skillId{0};       // TODO: 나중에 스킬 시스템으로 확장
+    DamageType type{DamageType::None};
+    DamageSource source{DamageSource::Unknown};
+    
+    // 필요 시 확장
+};
+
+struct DamageResult
+{
+    int32 requested{0};
+    int32 applied{0};
+    int32 hpBefore{0};
+    int32 hpAfter{0};
+    bool killed{false};
+    bool accepted{false};
+};
