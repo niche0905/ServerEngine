7cae95c8fb2557ccd0596e5876d51eea701c8219
niche0905 <yudle537@gmail.com>
2026-02-03 00:23:53 +0900

    BaseComponent 작성
    Component에 대한 작성을 하기전에 포석을 깔아두는 용도로 BaseComponent 코드를 작성하였다
    모든 Component는 해당 Component를 가지는 Owner가 있기에 이를 ID로 가지게 한다

---
+++ TimeThiefServer/Content/Component/BaseComponent.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+#include "Content/Object/ObjectId.h"
+
+struct ObjectId;
+
+/*-----------------
+   BaseComponent
+-----------------*/
+//
+// BaseComponent는 모든 컴포넌트의 기본 클래스입니다.
+// 가상 소멸자를 가지지 않으며, 단순히 소유자 ObjectId를 관리합니다.
+// 따라서 Component 들은 동적 할당을 해선 안됩니다. (다형 삭제가 이루어지지 않음을 보장해야함)
+//
+
+class BaseComponent
+{
+public:
+    void SetOwner(ObjectId owner) { owner_ = owner; }
+    ObjectId GetOwner() const { return owner_; }
+    bool HasOwner() const { return static_cast<bool>(owner_); }
+    
+private:
+    ObjectId owner_{};
+    
+};
