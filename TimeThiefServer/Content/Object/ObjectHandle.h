7adcbd7c23a5f2d7c79e716629cdaf5d654d655f
niche0905 <yudle537@gmail.com>
2026-02-03 00:10:49 +0900

    ObjectHandle 작성
    이미 해제된 메모리를 참조 하지 않기 위해서
    Pointer를 직접 가지는 것이 아닌 Handle을 가지게 하여 안정성을 높일 수 있었다
    Handle은 내부적으로 ObjectId를 Wrapping한 것으로 간단하게 작성된다

---
+++ TimeThiefServer/Content/Object/ObjectHandle.h
@@ -0,0 +1,48 @@
+﻿#pragma once
+#include "ObjectId.h"
+#include "ObjectManager.h"
+
+struct ObjectId;
+class ObjectManager;
+class BaseObject;
+
+/*----------------
+   ObjectHandle
+----------------*/
+//
+// ObjectHandle은 대상을 Pointer로 직접 참조하지 않고 간접적으로 참조하는 핸들입니다.
+// 서버에서 흔한 유스 애프터 프리 버그를 방지하기 위해 사용됩니다.
+//
+
+class ObjectHandle
+{
+public:
+   ObjectHandle() = default;
+   explicit ObjectHandle(ObjectId id) : id_(id) {}
+   
+   ObjectId GetId() const { return id_; }
+   bool IsValid() const { return static_cast<bool>(id_); }
+   
+   BaseObject* Resolve(ObjectManager& om) const
+   {
+      return om.Find(id_);
+   }
+   
+   template<typename T>
+   T* ResolveAs(ObjectManager& om) const
+   {
+      static_assert(std::is_base_of_v<BaseObject, T>);
+      return om.FindAs<T>(id_);
+   }
+   
+   void Reset() { id_ = {}; }
+   
+   bool operator==(const ObjectHandle& rhs) const { return id_ == rhs.id_; }
+   bool operator!=(const ObjectHandle& rhs) const { return id_ != rhs.id_; }
+   
+   explicit operator bool() const { return IsValid(); }
+   
+private:
+   ObjectId id_{};
+   
+};
