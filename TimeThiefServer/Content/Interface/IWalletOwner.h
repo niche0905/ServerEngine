f44d3eb817cf71b2a35806319e8b155b44afdb41
niche0905 <yudle537@gmail.com>
2026-02-06 16:19:37 +0900

    IWalletOwner 인터페이스 작성
    재화를 가져야하는 객체가 구현해야할 것들을 WalletOwner로 작성하였다

---
+++ TimeThiefServer/Content/Interface/IWalletOwner.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+#include "Content/Enum/WalletTypes.h"
+
+class ObjectManager;
+
+/*----------------
+   IWalletOwner
+----------------*/
+//
+// IWalletOwner는 지갑 소유자가 구현해야 하는 인터페이스입니다.
+//
+
+class IWalletOwner
+{
+public:
+    virtual ~IWalletOwner() = default;
+
+    virtual int64 GetBalance(CurrencyId currency) const = 0;
+    virtual bool CanSpend(CurrencyId currency, int64 amount) const = 0;
+
+    virtual MoneyChangeResult AddMoney(ObjectManager& om,
+                                       CurrencyId currency,
+                                       int64 amount,
+                                       const MoneyChangeContext& ctx) = 0;
+
+    virtual MoneyChangeResult SpendMoney(ObjectManager& om,
+                                         CurrencyId currency,
+                                         int64 amount,
+                                         const MoneyChangeContext& ctx) = 0;
+};
