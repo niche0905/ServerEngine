c5b1b0a16bdfa65352f24cb06203f9bc04ddcb9f
niche0905 <yudle537@gmail.com>
2026-02-06 16:14:54 +0900

    WalletTypes 정의
    wallet(재화)에 사용될 기초적인 enum과 struct 들을 정의하였다

---
+++ TimeThiefServer/Content/Enum/WalletTypes.h
@@ -0,0 +1,42 @@
+﻿#pragma once
+
+using CurrencyId = uint16;
+
+enum class CurrencyType : CurrencyId
+{
+    TimePoint = 1,
+    // Gold,
+    // Silver,
+    // Gem,
+};
+
+enum class MoneyChangeReason : uint8
+{
+    Unknown = 0,
+    Loot,
+    DropOnDeath,
+    // QuestReward,
+    Purchase,
+    // Sell,
+    Cheat,
+    System,
+};
+
+struct MoneyChangeContext
+{
+    MoneyChangeReason reason{MoneyChangeReason::Unknown};
+
+    // 선택적 추적 정보
+    int32 relatedItemId{0};
+    // ObjectId source; // 필요하면 공격자/몬스터 등
+};
+
+struct MoneyChangeResult
+{
+    CurrencyId currency{0};
+    int64 before{0};
+    int64 after{0};
+    int64 delta{0};
+
+    bool accepted{false}; // 변경이 실제로 적용되었는지
+};
