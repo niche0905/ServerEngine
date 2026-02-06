b607949d3e5f4006fd2767fa4e70f0e5c67f446b
niche0905 <yudle537@gmail.com>
2026-02-06 20:09:13 +0900

    LootTableService 정의
    LootTableService의 인터페이스를 작성하였고 구현체는 아직이다

---
+++ TimeThiefServer/Content/Service/LootTableService.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "LootTableService.h"
+
+/*--------------------
+   LootTableService
+--------------------*/
+
+bool LootTableService::LoadFromFile(const std::string& filepath)
+{
+}
+
+bool LootTableService::Reload()
+{
+}
+
+LootBundle LootTableService::Roll(int32 tableId, uint32 rngSeed, const LootRollContext& ctx) const
+{
+}
+
+bool LootTableService::HasTable(int32 tableId) const
+{
+}
---
+++ TimeThiefServer/Content/Service/LootTableService.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+#include "Content/Enum/LootTypes.h"
+#include "Runtime/Config/IConfigLoader.h"
+#include <string>
+#include <unordered_map>
+
+struct LootTableDef
+{
+    int32 tableId{0};
+};
+
+/*--------------------
+   LootTableService
+--------------------*/
+//
+// LootTableService는 루팅 테이블을 관리합니다.
+//
+
+class LootTableService
+{
+public:
+    explicit LootTableService(SE::Config::IConfigLoader& loader)
+        : configLoader_(loader)
+    {}
+    
+    bool LoadFromFile(const std::string& filepath);
+    bool Reload();
+    
+    LootBundle Roll(int32 tableId, uint32 rngSeed, const LootRollContext& ctx) const;
+    
+    bool HasTable(int32 tableId) const;
+    
+private:
+    SE::Config::IConfigLoader& configLoader_;
+    std::string lastPath;
+    
+    std::unordered_map<int32, LootTableDef> tables_;
+    
+};
