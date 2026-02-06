af8ca3b128335506de570931dac720bdd98225db
niche0905 <yudle537@gmail.com>
2026-02-06 16:50:48 +0900

    IInventoryOwner 인터페이스 작성
    Inventory를 가질 class 가 구현해야 할 제약을 적어둔 인터페이스를 작성하였다

---
+++ TimeThiefServer/Content/Interface/IInventoryOwner.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+#include "Content/Enum/ItemTypes.h"
+
+class ObjectManager;
+
+/*-------------------
+   IInventoryOwner
+-------------------*/
+//
+// IInventoryOwner는 인벤토리 소유자가 구현해야 하는 인터페이스입니다.
+//
+
+class IInventoryOwner
+{
+public:
+    virtual ~IInventoryOwner() = default;
+
+    virtual int32 GetCapacity() const = 0;
+    virtual int32 GetUsedSlots() const = 0;
+
+    virtual int32 GetItemCount(ItemId itemId) const = 0;
+
+    virtual InventoryOpResult AddItem(ObjectManager& om, ItemId itemId, int32 count, const ItemChangeContext& ctx) = 0;
+    virtual InventoryOpResult RemoveItem(ObjectManager& om, ItemId itemId, int32 count, const ItemChangeContext& ctx) = 0;
+
+    // 소비(=Remove의 의미 확정 버전)
+    virtual InventoryOpResult ConsumeItem(ObjectManager& om, ItemId itemId, int32 count, const ItemChangeContext& ctx) = 0;
+};
