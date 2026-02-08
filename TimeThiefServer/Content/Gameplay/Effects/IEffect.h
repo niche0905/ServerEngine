bc3376b71e8f9cb7dbda5992385fb197f7a7ce31
niche0905 <yudle537@gmail.com>
2026-02-08 22:18:43 +0900

    IEffect 작성
    Effect의 인터페이스를 작성하였다

---
+++ TimeThiefServer/Content/Gameplay/Effects/IEffect.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+#include "EffectTypes.h"
+
+class ObjectManager;
+
+/*-----------
+   IEffect
+-----------*/
+//
+// IEffect는 게임 내에서 효과(버프/디버프 등)를 구현하는 인터페이스입니다.
+//
+
+class IEffect
+{
+public:
+    virtual ~IEffect() = default;
+    
+    virtual const EffectDef& GetDef() const = 0;
+    
+    virtual void OnApply(ObjectManager& om, ObjectId target, const EffectApplyContext& ctx) = 0;
+    virtual void OnRefresh(ObjectManager& om, ObjectId target, const EffectApplyContext& ctx) = 0;
+    virtual void OnRemove(ObjectManager& om, ObjectId target, uint64 nowMs) = 0;
+    
+    virtual void OnExpired(ObjectManager& om, ObjectId target, uint64 nowMs)
+    {
+        (void)om; (void)target; (void)nowMs;
+        // 기본 구현은 아무 것도 하지 않음
+    }
+    
+};
