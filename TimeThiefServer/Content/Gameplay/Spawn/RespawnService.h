766b48640153e7c1b258906b1f1908593d6530d4
niche0905 <yudle537@gmail.com>
2026-02-08 21:52:15 +0900

    RespawnService 작성
    간단한 기능만을 내포하는 RespawnService를 작성하였다
    추후 확장을 해야 한다

---
+++ TimeThiefServer/Content/Gameplay/Spawn/RespawnService.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+#include "RespawnComponent.h"
+
+class ObjectManager;
+class RespawnComponent;
+class IRespawnOwner;
+struct RespawnContext;
+
+/*------------------
+   RespawnService
+------------------*/
+//
+// RespawnService는 리스폰 처리를 담당하는 서비스 클래스입니다.
+//
+
+class RespawnService
+{
+public:
+    // THINK: static 멤버로 할지 인스턴스 멤버로 할지 고민
+    //        멤버로 respawn 필요한 객체를 가지게 할 수도 있다
+    bool TryExecute(ObjectManager& om, RespawnComponent& comp, IRespawnOwner& owner, const RespawnContext& ctx)
+    {
+        if (not comp.IsDue(ctx.nowMs)) return false;
+        
+        return comp.ExecuteRespawn(om, owner, ctx);
+    }
+    
+};
