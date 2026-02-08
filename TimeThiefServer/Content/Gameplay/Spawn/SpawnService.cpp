efed551b83519f2dbb91b91dd3b4de6fb9e7fb2a
niche0905 <yudle537@gmail.com>
2026-02-08 23:51:47 +0900

    SpawnService 초안 작성
    SpawnService의 초안을 작성하였다
    하지만 개선의 필요성을 느끼지만 당장에 떠오르는 방법이 없어서 여기까지만 작성하기로 하였다

---
+++ TimeThiefServer/Content/Gameplay/Spawn/SpawnService.cpp
@@ -0,0 +1,34 @@
+﻿#include "pch.h"
+#include "SpawnService.h"
+
+/*-----------------
+   SpawnService
+-----------------*/
+
+int32 SpawnService::InitialSpawn(ObjectManager& om, uint64 nowMs)
+{
+}
+
+void SpawnService::Update(ObjectManager& om, uint64 nowMs)
+{
+}
+
+void SpawnService::NotifyDead(int32 spawnPointId, ObjectId obj, uint64 nowMs)
+{
+}
+
+int32 SpawnService::DespawnAll(ObjectManager& om, DespawnReason reason, uint64 nowMs)
+{
+}
+
+bool SpawnService::TrySpawnOne(ObjectManager& om, const SpawnPoint& sp, uint64 nowMs)
+{
+}
+
+void SpawnService::UpdateRespawn(ObjectManager& om, uint64 nowMs)
+{
+}
+
+void SpawnService::UpdateDespawn(ObjectManager& om, uint64 nowMs)
+{
+}
---
+++ TimeThiefServer/Content/Gameplay/Spawn/SpawnService.h
@@ -0,0 +1,56 @@
+﻿#pragma once
+#include "SpawnTypes.h"
+#include "RespawnTypes.h"
+#include <unordered_map>
+#include <vector>
+
+class ObjectManager;
+struct SE::Math::Vector3;
+class ISpawnFactory;
+
+/*-----------------
+   SpawnService
+-----------------*/
+//
+// SpawnService는 스폰 관련 기능을 제공하는 서비스입니다.
+//
+
+class SpawnService
+{
+public:
+   using Vector3 = SE::Math::Vector3;
+   
+public:
+   explicit SpawnService(ISpawnFactory& factory)
+      : factory_(factory)
+   {
+   }
+   
+   void AddSpawnPoint(const SpawnPoint& sp)
+   {
+      spawnPoints_[sp.id] = sp;
+   }
+   
+   bool HasSpawnPoint(int32 id) const
+   {
+      return spawnPoints_.find(id) != spawnPoints_.end();
+   }
+   
+   int32 InitialSpawn(ObjectManager& om, uint64 nowMs);
+   
+   void Update(ObjectManager& om, uint64 nowMs);
+   
+   void NotifyDead(int32 spawnPointId, ObjectId obj, uint64 nowMs);
+   
+   int32 DespawnAll(ObjectManager& om, DespawnReason reason, uint64 nowMs);
+   
+private:
+   bool TrySpawnOne(ObjectManager& om, const SpawnPoint& sp, uint64 nowMs);
+   void UpdateRespawn(ObjectManager& om, uint64 nowMs);
+   void UpdateDespawn(ObjectManager& om, uint64 nowMs);
+   
+private:
+   ISpawnFactory& factory_;
+   std::unordered_map<int32, SpawnPoint> spawnPoints_;      // 스폰 포인트 ID별 정의
+    
+};
