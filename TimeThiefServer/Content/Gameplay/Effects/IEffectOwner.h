4582243e73d61f260167cc23b8bd2dfdf312ddb2
niche0905 <yudle537@gmail.com>
2026-02-08 22:22:11 +0900

    IEffectOwner 작성
    효과를 받을 수 있는 객체가 구현해야 할 계약을 인터페이스로 작성하였다

---
+++ TimeThiefServer/Content/Gameplay/Effects/IEffectOwner.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+#include "Content/Object/ObjectId.h"
+
+class ObjectManager;
+class HealthComponent;
+class WalletComponent;
+class InventoryComponent;
+class EffectComponent;
+
+/*----------------
+   IEffectOwner
+----------------*/
+//
+// IEffectOwner는 효과(버프/디버프 등)를 소유하는 객체를 나타내는 인터페이스입니다.
+//
+
+class IEffectOwner
+{
+public:
+    virtual ~IEffectOwner() = default;
+    
+    virtual ObjectId GetId() const = 0;
+    
+    virtual EffectComponent& GetEffectComponent() = 0;
+    
+    virtual HealthComponent* TryGetHealth() = 0;        // 체력 컴포넌트 (있을 수도 있고 없을 수도 있음)
+    
+    // MEMO: 필요해지면 점진적으로 추가
+    
+    virtual void OnEffectChanged() {};
+    
+};
