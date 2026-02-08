aa22a823e24514b51110996e2cd13a5f7afed206
niche0905 <yudle537@gmail.com>
2026-02-08 23:32:49 +0900

    ISpawnFactory 작성
    SpawnFactory 인터페이스로 실제 객체 생성을 컨텐츠 코드에 위임하는 방식을 채택하였다

---
+++ TimeThiefServer/Content/Gameplay/Spawn/ISpawnFactory.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include "SpawnTypes.h"
+
+class ObjectManager;
+struct SE::Math::Vector3;
+
+/*-----------------
+   ISpawnFactory
+-----------------*/
+//
+// ISpawnFactory는 스폰 요청을 처리하는 팩토리 인터페이스입니다.
+//
+
+class ISpawnFactory
+{
+public:
+    using Vector3 = SE::Math::Vector3;
+    
+public:
+    virtual ~ISpawnFactory() = default;
+    
+    virtual SpawnResult Spawn(ObjectManager& om, const SpawnRequest& req, const Vector3& position, uint64 nowMs) = 0;
+    
+};
