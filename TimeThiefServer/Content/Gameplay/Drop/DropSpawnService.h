b569e0fbbd9ef3f3bc5ca390061d9c75120f2bff
niche0905 <yudle537@gmail.com>
2026-02-08 20:59:39 +0900

    DropSpawnService 작성
    DropSpawnService의 초안인 인터페이스를 작성하였다
    구현체는 Object 구조에서 필요한 Object와 Object 생성 관련한 것들을 우선적으로 만들고 작성해야 한다

---
+++ TimeThiefServer/Content/Gameplay/Drop/DropSpawnService.h
@@ -0,0 +1,43 @@
+﻿#pragma once
+#include "Content/Gameplay/Drop/DropTypes.h"
+#include "Content/Gameplay/Loot/LootTypes.h"
+#include "Content/Object/ObjectId.h"
+
+class ObjectManager;
+struct SE::Math::Vector3;
+
+struct DropSpawnContext
+{
+    ObjectId owner{};       // 드롭을 생성하는 오브젝트 (죽은 플레이어/몬스터)
+    ObjectId instigator{};  // 킬러/원인
+    uint64 nowMs{0};
+};
+
+struct DropSpawnResult
+{
+    bool spawned{false};
+    ObjectId spawnedActor{};    // 생성된 드롭 액터의 ID, CorpseBox면 해당 actor id, Scatter라면 대표 값이 없을 수 있음
+    int32 spawnedCount{0};      // 생성된 드롭 아이템의 개수
+};
+
+/*---------------------
+   DropSpawnService
+---------------------*/
+//
+// DropSpawnService
+//
+
+class DropSpawnService
+{
+public:
+    using Vector3 = SE::Math::Vector3;
+    
+    // TODO: 각자 필요한 Actor들 만들고 구현체 작성하기
+public:
+    DropSpawnResult Spawn(ObjectManager& om, const LootBundle& bundle, const DropSpawnPolicy& policy, const Vector3& position, const DropSpawnContext& ctx);
+    
+private:
+    DropSpawnResult SpawnCorpseBox(ObjectManager& om, const LootBundle& bundle, const Vector3& position, const DropSpawnContext& ctx);
+    DropSpawnResult SpawnScatter(ObjectManager& om, const LootBundle& bundle, const DropSpawnPolicy& policy, const Vector3& position, const DropSpawnContext& ctx);
+    
+};
