6a20585d3e9ff484a82e57eb8787c9edb8bc8f51
niche0905 <yudle537@gmail.com>
2026-02-08 22:55:36 +0900

    CooldownTypes 작성
    CooldownTypes를 가볍게 정의하였다

---
+++ TimeThiefServer/Content/Gameplay/Cooldown/CooldownTypes.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+using CooldownId = uint32;
+
+enum class CooldownStartMode : uint8
+{
+    FromNow = 0,    // 현재 시점부터 쿨다운 시작
+    FromEnd,        // 기존 쿨다운 종료 시점부터 연장
+};
+
+struct CooldownResult
+{
+    bool ok{false};
+    uint64 nowMs{0};
+    uint64 endMs{0};
+    uint64 remainingMs{0};
+};
