e54e394637f8265f39e88b2b793632e7b733c3c4
niche0905 <yudle537@gmail.com>
2026-02-09 22:34:36 +0900

    Actor 작성
    BaseObject를 상속받는 Actor를 작성하였다
    큰 특징으론 Transform 정보를 담고 있어야 한다는 것이다
    아직 미흡한 부분이 있다

---
+++ TimeThiefServer/Content/Object/Actor/Actor.cpp
@@ -0,0 +1,54 @@
+﻿#include "pch.h"
+#include "Actor.h"
+#include "Content/Gameplay/Replication/ReplicationSystem.h"
+#include "Content/Gameplay/Spawn/SpawnService.h"
+
+/*-----------------
+   Local Helper
+-----------------*/
+
+namespace 
+{
+    // TODO: 구현 필요
+    
+    ObjectManager* TryGetObjectManager(Actor::RoomId /*roomId*/)
+    {
+        return nullptr;
+    }
+    
+    SpawnService* TryGetSpawnService(Actor::RoomId /*roomId*/)
+    {
+        return nullptr;
+    }
+    
+    ReplicationSystem* TryGetReplication(Actor::RoomId /*roomId*/)
+    {
+        return nullptr;
+    }
+}
+
+/*---------
+   Actor
+---------*/
+
+void Actor::SetPosition(const Vector3& position)
+{
+    position_ = position;
+    OnRepDirtyTransform();
+}
+
+void Actor::OnRepDirtyTransform()
+{
+    if (auto* rep = TryGetReplication(GetRoomId())) {
+        rep->MarkDirty(GetId(), RepField::Transform);
+    }
+}
+
+void Actor::RegisterLifetimeMs(uint64 nowMs, uint32 lifetimeMs)
+{
+    if (lifetimeMs == 0) return;
+    
+    if (auto* ss = TryGetSpawnService(GetRoomId())) {
+        ss->RegisterLifetimeMs(GetId(), nowMs, lifetimeMs);
+    }
+}
---
+++ TimeThiefServer/Content/Object/Actor/Actor.h
@@ -0,0 +1,54 @@
+﻿#pragma once
+#include "Content/Object/BaseObject.h"
+#include "Content/Object/ObjectId.h"
+#include <vector>
+#include <memory>
+
+struct SE::Math::Vector3;
+class ObjectManager;
+class SpawnService;
+class ReplicationSystem;
+
+/*---------
+   Actor
+---------*/
+//
+// Actor는 게임 내에서 활동하는 모든 개체를 나타내는 기본 클래스입니다.
+// Transform 개념을 가지며, Replicaiton 대상입니다
+// Component를 가질 수 있습니다
+// Spawn/Despawn 라이프사이클 hook을 제공합니다
+//
+
+class Actor : public BaseObject
+{
+public:
+   using Vector3 = SE::Math::Vector3;
+   
+public:
+   Actor() = default;
+   virtual ~Actor() = default;
+   
+   Actor(const Actor&) = delete;
+   Actor& operator=(const Actor&) = delete;
+   
+// Transform 관련 함수들
+public:
+   const Vector3& GetPosition() const { return position_; }
+   void SetPosition(const Vector3& position);
+   
+// Replication 관련 함수들
+public:
+   virtual bool IsReplicated() const { return true; } // 기본적으로 모든 Actor는 Replicated 대상입니다. 예외 시 재정의
+   
+protected:
+   virtual void OnRepDirtyTransform();
+   
+protected:
+   void RegisterLifetimeMs(uint64 nowMs, uint32 lifetimeMs);   // 파생 클래스가 수명이 있는 Actor일 경우 호출하여 수명 등록
+   
+private:
+   Vector3 position_{};
+   
+   // TODO: 추가하기 (회전, 스케일 등 Transform 관련 정보 들)
+    
+};
