8085c865a657a119995689a7859cbf82f68a196c
niche0905 <yudle537@gmail.com>
2026-02-09 21:06:18 +0900

    ReplicationTypes 작성
    RepTypes로 추후 필요한 타입들을 정의하였다

---
+++ TimeThiefServer/Content/Gameplay/Replication/ReplicationTypes.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include "Content/Object/ObjectId.h"
+
+struct SE::Math::Vector3;
+
+using RepObjectId = ObjectId;
+using ClientId = uint32;
+using TickSeq = uint32;
+
+struct RepFrame
+{
+    TickSeq seq{0};
+    uint64 nowMs{0};
+};
+
+enum class RepChannel : uint8
+{
+    Spawn = 0,
+    Despawn,
+    State,
+    Event,
+};
