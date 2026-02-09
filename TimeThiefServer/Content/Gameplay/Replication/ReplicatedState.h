1db784bb24df26ca0de5ce43102498e1bd8f38dc
niche0905 <yudle537@gmail.com>
2026-02-09 21:19:13 +0900

    ReplicatedState 작성
    현재 Rep 관련 상태를 저장하는 구조체를 작성하였다

---
+++ TimeThiefServer/Content/Gameplay/Replication/ReplicatedState.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include "ReplicationMask.h"
+#include "ReplicationTypes.h"
+
+struct ReplicatedState
+{
+    RepObjectId object{};
+    RepMask dirtyMask{0};
+    
+    TickSeq lastFlushSeq{0};    // 마지막으로 flush한 Tick
+    
+    bool IsDirty() const { return dirtyMask != 0; }
+    
+    void MarkDirty(RepField f) { RepMaskSet(dirtyMask, f); }
+    void ClearDirty(RepField f) { dirtyMask = 0; }  // 전체 초기화
+};
