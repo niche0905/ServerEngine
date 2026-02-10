6425ffd286612930f69dcce2b310dc9d53e3c59f
niche0905 <yudle537@gmail.com>
2026-02-10 23:48:39 +0900

    ProjectileActor 작성
    로켓 런쳐의 로켓을 담당할 것을 생각하며 투사체 코드를 작성하였다

---
+++ TimeThiefServer/Content/Object/Actor/ProjectileActor.cpp
@@ -0,0 +1,53 @@
+﻿#include "pch.h"
+#include "ProjectileActor.h"
+
+/*-------------------
+   ProjectileActor
+-------------------*/
+
+void ProjectileActor::Init(ObjectId ownerId, const Vector3& startPos, const Vector3& velocity, int32 damage,
+    uint32 lifetimeMs)
+{
+    ownerId_ = ownerId;
+    damage_ = damage;
+    velocity_ = velocity;
+    lifetimeMs_ = lifetimeMs;
+    
+    SetPosition(startPos);
+}
+
+void ProjectileActor::OnSpawn()
+{
+    Actor::OnSpawn();
+    
+    // TODO: Register Lifetime 등록하기
+    // RegisterLifetimeMs(nowMs, delay)
+}
+
+void ProjectileActor::Tick(float dt)
+{
+    Actor::Tick(dt);
+    
+    Vector3 pos = GetPosition();
+    pos.x += velocity_.x * dt;
+    pos.y += velocity_.y * dt;
+    pos.z += velocity_.z * dt;
+    SetPosition(pos);
+    
+    // TODO: 충돌 검사 및 OnHit 호출
+    // Spatial/Collider 시스템 필요
+}
+
+
+void ProjectileActor::OnHit(ObjectManager& om, ObjectId hitObjectId)
+{
+    (void)om;
+    (void)hitObjectId;
+ 
+    // TODO: 아래 동작 구현
+    // 1) 데미지 계산 식에 맞게 적용 (범위, 방어구 등)
+    // 2) 자신은 제거 필요 시 EffectArea 생성 등 추가 동작
+    
+    // 기본적으로 충돌 시 파괴
+    // __RequestDestroy();
+}
---
+++ TimeThiefServer/Content/Object/Actor/ProjectileActor.h
@@ -0,0 +1,54 @@
+﻿#pragma once
+#include "Actor.h"
+
+class ObjectManager;
+
+/*-------------------
+   ProjectileActor
+-------------------*/
+//
+// ProjectileActor는 발사체(총알, 미사일 등)를 나타내는 클래스입니다.
+// 발사체는 소유자, 데미지, 속도, 수명 등의 속성을 가지며,
+// 충돌 시 특정 동작을 수행할 것을 예상하여 설계되었습니다.
+//
+
+class ProjectileActor : public Actor
+{
+public:
+    ProjectileActor() = default;
+    virtual ~ProjectileActor() = default;
+    
+    ProjectileActor(const ProjectileActor&) = delete;
+    ProjectileActor& operator=(const ProjectileActor&) = delete;
+    
+public:
+    ObjectId GetOwner() const { return ownerId_; }
+    void SetOwner(ObjectId ownerId) { ownerId_ = ownerId; }
+    
+    int32 GetDamage() const { return damage_; }
+    void SetDamage(int32 damage) { damage_ = damage; }
+    
+    const Vector3& GetVelocity() const { return velocity_; }
+    void SetVelocity(const Vector3& velocity) { velocity_ = velocity; }
+    
+    uint32 GetLifetimeMs() const { return lifetimeMs_; }
+    void SetLifetimeMs(uint32 lifetimeMs) { lifetimeMs_ = lifetimeMs; }
+    
+public:
+    void Init(ObjectId ownerId, const Vector3& startPos, const Vector3& velocity, int32 damage, uint32 lifetimeMs);
+    
+protected:
+    virtual void OnSpawn() override;
+    virtual void Tick(float dt) override;
+    
+protected:
+    virtual void OnHit(ObjectManager& om, ObjectId hitObjectId);
+    
+private:
+    ObjectId ownerId_;
+    int32 damage_{ 0 };
+    
+    Vector3 velocity_{};
+    uint32 lifetimeMs_{ 0 };
+    
+};
