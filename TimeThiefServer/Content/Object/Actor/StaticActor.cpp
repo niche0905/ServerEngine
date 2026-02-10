c9b32d9d18ea601df3049478ebd7e28f261d0ab2
niche0905 <yudle537@gmail.com>
2026-02-10 15:56:41 +0900

    StaticActor 작성
    간단한 StaticActor를 정의하였다

---
+++ TimeThiefServer/Content/Object/Actor/StaticActor.cpp
@@ -0,0 +1,18 @@
+﻿#include "pch.h"
+#include "StaticActor.h"
+
+/*---------------
+   StaticActor
+---------------*/
+
+void StaticActor::OnSpawn()
+{
+   Actor::OnSpawn();
+}
+
+void StaticActor::Tick(float dt)
+{
+   (void)dt;
+   // StaticActor는 Tick 시 별도의 동작이 없습니다.
+   // 필요 시 파생 클래스에서 재정의할 수 있습니다.
+}
---
+++ TimeThiefServer/Content/Object/Actor/StaticActor.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+#include "Actor.h"
+
+/*---------------
+   StaticActor
+---------------*/
+//
+// StaticActor는 고정된 World Object의 기본 클래스입니다
+// 기본적으로 움직이지 않고,
+// 상호작용이 가능한 상자/문/레버 등의 오브젝트를 구현할 때 사용됩니다.
+//
+
+class StaticActor : public Actor
+{
+public:
+   StaticActor() = default;
+   virtual ~StaticActor() = default;
+   
+   StaticActor(const StaticActor&) = delete;
+   StaticActor& operator=(const StaticActor&) = delete;
+   
+public:
+   bool IsStatic() const { return true; }
+
+protected:
+   void SetPositionStatic(const Vector3& position) { Actor::SetPosition(position); }
+   
+protected:
+   virtual void OnSpawn() override;
+   virtual void Tick(float dt) override;
+    
+};
