188769a50de7f05a67b30d6a9faa01f17e5b38de
Niche <73772820+niche0905@users.noreply.github.com>
2026-02-27 21:15:05 +0900

    Player 정의
    Player을 얇게 영속성을 유지할 수 있게 하는 객체로 디자인하였다

---
+++ TimeThiefServer/Content/Player/Player.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+#include "PlayerState.h"
+
+using PlayerId = uint64;
+using SessionId = uint64;
+using PawnId = uint32;
+
+/*----------
+   Player
+----------*/
+//
+// Player는 게임 플레이어를 나타냅니다.
+// 얇게 구현된 객체로, 플레이어의 고유 ID, 세션 ID, 조종하는 Pawn의 ID, 그리고 플레이어 상태를 포함합니다.
+//
+
+class Player
+{
+public:
+    PlayerId id_ = 0;               // 플레이어 고유 ID (DB ID)
+    SessionId sessionId_ = 0;       // 플레이어와 연결된 세션 ID
+    PawnId pawnId_ = 0;             // 플레이어가 조종하는 Pawn의 ID
+    
+    PlayerState state_;
+    
+};
---
+++ TimeThiefServer/Content/Player/PlayerState.h
@@ -0,0 +1,10 @@
+#pragma once
+
+struct PlayerState
+{
+    // 영속성을 가지는 정보들
+    // ex) 플레이어 이름, 레벨, 경험치 등등...
+    
+    int32 level = 1;
+    int32 exp = 0;
+};
