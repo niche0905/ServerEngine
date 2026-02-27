c0f125f7d84fc013d14e84e339ceac905ec9a211
Niche <73772820+niche0905@users.noreply.github.com>
2026-02-27 20:54:08 +0900

    Room 초안 작성
    Room에 필요한 것들을 기본적으로 작성하였다
    Player를 우선적으로 작업할 필요를 느껴서 먼저 작성하고 다시 돌아와서 진행해야 할 듯 싶다

---
+++ TimeThiefServer/Content/Room/Room.cpp
@@ -0,0 +1,46 @@
+﻿#include "pch.h"
+#include "Room.h"
+#include "Content/Object/BaseObject.h"
+
+/*---------
+   Room
+---------*/
+
+// TEMP: 임시로 전역으로 생성, 추후 Service와 Thread와 맞게 구조 변경 필요
+std::shared_ptr<Room> GRoom = std::make_shared<Room>(1);
+
+Room::Room(uint32 roomId)
+   : roomId_(roomId)
+   , objectManager_(roomId)
+{
+}
+
+Room::~Room()
+{
+}
+
+bool Room::EnterRoom(std::shared_ptr<BaseObject> object)
+{
+   bool success = AddObject(object);
+   
+   // TODO: Player 먼저
+}
+
+bool Room::LeaveRoom(std::shared_ptr<BaseObject> object)
+{
+   if (object == nullptr) return false;
+   
+   const ObjectId id = object->GetId();
+   bool success = RemoveObject(id);
+   
+   // TODO: Player 먼저
+}
+
+void Room::UpdateTick()
+{
+}
+
+std::shared_ptr<Room> Room::GetRoomRef()
+{
+   return static_pointer_cast<Room>(shared_from_this());
+}
---
+++ TimeThiefServer/Content/Room/Room.h
@@ -0,0 +1,49 @@
+﻿#pragma once
+#include "Content/Object/ObjectManager.h"
+
+class BaseObject;
+class ObjectManager;
+class Player;
+
+/*---------
+   Room
+---------*/
+//
+// Room는 게임 플레이어들이 함께 상호작용하는 공간을 나타냅니다. 
+// 각 Room은 고유한 ID를 가지며, 플레이어와 게임 오브젝트들이 존재할 수 있습니다. 
+// Room은 게임 로직과 상태를 관리하며, 플레이어 간의 상호작용을 중개하는 역할을 합니다.
+//
+
+class Room : public std::enable_shared_from_this<Room>
+{
+public:
+   explicit Room(uint32 roomId);
+   ~Room();
+   
+public:
+   bool EnterRoom(std::shared_ptr<BaseObject> object);
+   bool LeaveRoom(std::shared_ptr<BaseObject> object);
+   
+   // TODO: Player 객체 만들기
+   bool HandleEnterPlayer(std::shared_ptr<Player> player);
+   bool HandleLeavePlayer(std::shared_ptr<Player> player);
+   
+public:
+   void UpdateTick();
+   
+   std::shared_ptr<Room> GetRoomRef();
+   
+private:
+   bool AddObject(std::shared_ptr<BaseObject> object);
+   bool RemoveObject(ObjectId objectId);
+   
+private:
+   void Broadcast(std::shared_ptr<SendBuffer>, uint32 exceptId = 0);
+   
+private:
+   uint32 roomId_;
+   ObjectManager objectManager_;
+    
+};
+
+extern std::shared_ptr<Room> GRoom;
