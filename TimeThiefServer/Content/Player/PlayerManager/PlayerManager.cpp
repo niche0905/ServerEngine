b30261a4f647832135e6d93b16bc5ecd7c1cfd3a
niche0905 <yudle537@gmail.com>
2026-03-02 22:59:40 +0900

    PlayerManager 구성
    PlayerManager를 간단하게 구성하였다
    Read(Find) 작업이 많을 것이라 예상하기에 shared_mutex(R/W lock)을 사용하였다

---
+++ TimeThiefServer/Content/Player/PlayerManager/PlayerManager.cpp
@@ -0,0 +1,76 @@
+﻿#include "pch.h"
+#include "PlayerManager.h"
+
+/*-----------------
+   PlayerManager
+-----------------*/
+
+PlayerManager::PlayerRef PlayerManager::Create(PlayerId playerId)
+{
+   // write lock
+   std::unique_lock<std::shared_mutex> lock(mutex_);
+   
+   auto it = playersById_.find(playerId);
+   if (it != playersById_.end())
+      return it->second;
+   
+   // TODO: Player 초기화 제대로 하기 (Player 한정자 변경)
+   PlayerRef newPlayer = std::make_shared<Player>();
+   playersById_.emplace(playerId, newPlayer);
+   
+   return newPlayer;
+}
+
+void PlayerManager::Remove(PlayerId playerId)
+{
+   // write lock
+   std::unique_lock<std::shared_mutex> lock(mutex_);
+   
+   playersById_.erase(playerId);
+}
+
+void PlayerManager::Clear()
+{
+   // write lock
+   std::unique_lock<std::shared_mutex> lock(mutex_);
+   
+   playersById_.clear();
+}
+
+PlayerManager::PlayerRef PlayerManager::Find(PlayerId playerId) const
+{
+   // read lock
+   std::shared_lock<std::shared_mutex> lock(mutex_);
+   
+   auto it = playersById_.find(playerId);
+   if (it == playersById_.end())
+      return nullptr;
+   
+   return it->second;
+}
+
+size_t PlayerManager::GetPlayerCount() const
+{
+   // read lock
+   std::shared_lock<std::shared_mutex> lock(mutex_);
+   
+   return playersById_.size();
+}
+
+std::vector<PlayerManager::PlayerRef> PlayerManager::SnapshotPlayers() const
+{
+   std::vector<PlayerRef> players;
+   
+   // read lock
+   std::shared_lock<std::shared_mutex> lock(mutex_);
+   players.reserve(playersById_.size());
+   
+   for (const auto& [id, player] : playersById_) {
+      if (player)
+         players.push_back(player);
+   }
+   
+   return players;
+}
+
+PlayerManager g_PlayerManager;
---
+++ TimeThiefServer/Content/Player/PlayerManager/PlayerManager.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+#include <shared_mutex>
+#include "Content/Player/Player.h"
+
+/*-----------------
+   PlayerManager
+-----------------*/
+//
+// PlayerManager는 모든 플레이어를 관리합니다.
+//
+
+class PlayerManager
+{
+public:
+   using PlayerRef = std::shared_ptr<Player>;
+   
+public:
+   PlayerRef Create(PlayerId playerId);      // 플레이어 없다면 생성/반환, 이미 존재하면 기존 플레이어 반환
+   void Remove(PlayerId playerId);
+   
+   void Clear();
+   
+   PlayerRef Find(PlayerId playerId) const;
+   size_t GetPlayerCount() const;
+   
+   std::vector<PlayerRef> SnapshotPlayers() const;
+   
+private:
+   mutable std::shared_mutex mutex_;                           // 플레이어 리스트 보호용 뮤텍스 (읽기가 많을 것으로 예상되어 shared_mutex 사용)
+   
+   std::unordered_map<PlayerId, PlayerRef> playersById_;       // 플레이어 ID -> 플레이어 참조
+    
+};
+
+extern PlayerManager g_PlayerManager;
