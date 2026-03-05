e64ab01fe8f45ebf75ef5a4b3790b561cec1077f
niche0905 <yudle537@gmail.com>
2026-03-05 21:06:00 +0900

    SessionIdMaker 작성
    간단한 atomic 자료구조를 통해서 ID 생성 로직을 작성하였다

---
+++ TimeThiefServer/Network/Session/SessionIdMaker.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+using SessionId = uint64;
+
+class SessionIdMaker
+{
+public:
+    static SessionId Next()
+    {
+        return nextSessionId.fetch_add(1, std::memory_order_relaxed);
+    }
+
+private:
+    // 0은 유효하지 않은 세션 ID로 간주하므로 1부터 시작
+    static inline std::atomic<SessionId> nextSessionId{1};
+    
+};
