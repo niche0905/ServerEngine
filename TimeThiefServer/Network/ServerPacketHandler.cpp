51d7a00108c082b2cf2283ed4a50249cbdfab3da
niche0905 <yudle537@gmail.com>
2026-03-05 23:31:14 +0900

    Protocol 수정
    Template 코드를 수정하여 PacketHandler의 구조가 바뀌었으며
    C_MoveInput을 서버 권위를 내려놓는 방식으로 수정하였다

--- TimeThiefServer/Network/ServerPacketHandler.cpp
+++ TimeThiefServer/Network/ServerPacketHandler.cpp
@@ -1,53 +1,58 @@
 ﻿#include "pch.h"
 #include "Generated/ServerPacketHandler.h"
 
+#include "Content/Room/Room.h"
+#include "Session/PlayerSession.h"
 
-bool Handle_C_HandshakeReq(PacketSessionRef& session, BYTE* buffer, int32 len)
+PacketHandlerFunc GPacketHandler[kMaxMessageId + 1];
+
+bool Handle_C_HandshakeReq(PacketSessionRef& session, const se::auth::C_HandshakeReq& pkt)
 {
     return false;
 }
 
-bool Handle_C_LoginReq(PacketSessionRef& session, BYTE* buffer, int32 len)
+bool Handle_C_LoginReq(PacketSessionRef& session, const se::auth::C_LoginReq& pkt)
 {
     return false;
 }
 
-bool Handle_C_Ping(PacketSessionRef& session, BYTE* buffer, int32 len)
+bool Handle_C_Ping(PacketSessionRef& session, const se::auth::C_Ping& pkt)
 {
     return false;
 }
 
-bool Handle_C_LobbyEnterReq(PacketSessionRef& session, BYTE* buffer, int32 len)
+bool Handle_C_LobbyEnterReq(PacketSessionRef& session, const se::lobby::C_LobbyEnterReq& pkt)
+{
+    // TEMP: 임시로 PlayerId와 SessionId 같게..
+    return GRoom->Join(session->Id(), session->Id());
+}
+
+bool Handle_C_MatchQueueEnterReq(PacketSessionRef& session, const se::lobby::C_MatchQueueEnterReq& pkt)
 {
     return false;
 }
 
-bool Handle_C_MatchQueueEnterReq(PacketSessionRef& session, BYTE* buffer, int32 len)
+bool Handle_C_MatchQueueCancelReq(PacketSessionRef& session, const se::lobby::C_MatchQueueCancelReq& pkt)
 {
     return false;
 }
 
-bool Handle_C_MatchQueueCancelReq(PacketSessionRef& session, BYTE* buffer, int32 len)
+bool Handle_C_RoomReadyReq(PacketSessionRef& session, const se::room::C_RoomReadyReq& pkt)
 {
     return false;
 }
 
-bool Handle_C_RoomReadyReq(PacketSessionRef& session, BYTE* buffer, int32 len)
+bool Handle_C_MoveInput(PacketSessionRef& session, const se::room::C_MoveInput& pkt)
+{
+    return GRoom->HandleMove(session->Id(), pkt);
+}
+
+bool Handle_C_AimInput(PacketSessionRef& session, const se::room::C_AimInput& pkt)
 {
     return false;
 }
 
-bool Handle_C_MoveInput(PacketSessionRef& session, BYTE* buffer, int32 len)
-{
-    return false;
-}
-
-bool Handle_C_AimInput(PacketSessionRef& session, BYTE* buffer, int32 len)
-{
-    return false;
-}
-
-bool Handle_C_FireReq(PacketSessionRef& session, BYTE* buffer, int32 len)
+bool Handle_C_FireReq(PacketSessionRef& session, const se::room::C_FireReq& pkt)
 {
     return false;
 }
