6ace7d71f6eac465729cb98bfdc126c0ae6c9046
niche0905 <yudle537@gmail.com>
2026-03-05 23:29:31 +0900

    Text 코드 제거
    Text를 보내기만 할 뿐인 코드를 지웠다

--- TimeThiefServer/Main/main.cpp
+++ TimeThiefServer/Main/main.cpp
@@ -17,14 +17,6 @@ constexpr int SERVER_PORT = 8252;	// TEMP
 
 ThreadManager threadManager;
 
-std::vector<std::string> testStrings = {
-	"Hello, World!",
-	"TimeThiefServer is running.",
-	"This is a test packet.",
-	"Networking with IOCP is powerful.",
-	"Have a great day!"
-};
-
 void WorkerJob(ServiceRef& service)
 {
 	while (true) {
@@ -56,7 +48,7 @@ int main()
 	consoleLogger->Log(Color::Green, L"[main] Start OK\n");
 
 	consoleLogger->Log(Color::Green, L"[main] before launching workers\n");
-	int32 hc = (int32)std::thread::hardware_concurrency();
+	int32 hc = static_cast<int32>(std::thread::hardware_concurrency());
 	int32 workerCount = (hc > 1) ? (hc - 1) : 1;
 	for (int32 i = 0; i <  workerCount; ++i) {
 		threadManager.Launch([&service]()
@@ -70,17 +62,6 @@ int main()
 	int32 testIndex = 0;
 	while (true) {
 		std::this_thread::sleep_for(std::chrono::seconds(1));
-		
-		std::string message = testStrings[testIndex];
-		testIndex = (testIndex + 1) % testStrings.size();
-		
-		std::shared_ptr<SendBuffer> sendBuffer = std::make_shared<SendBuffer>(message.size() + 1);
-		
-		uint8 size = static_cast<uint8>(message.size() + 1);
-		sendBuffer->OnWrite(reinterpret_cast<byte*>(&size), 1);
-		sendBuffer->OnWrite(reinterpret_cast<byte*>(message.data()), message.size());
-		
-		g_SessionManager.Broadcast(sendBuffer);
 	}
 	
 	threadManager.Join();
