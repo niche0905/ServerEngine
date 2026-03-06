3690724195150271039ba209ae18881ea40f80ee
niche0905 <yudle537@gmail.com>
2026-03-06 17:20:20 +0900

    ServerfConfigReader 작성
    Config 값을 읽어들이는 class를 하나 만들었다

---
+++ TimeThiefServer/Network/ServerConfigReader.cpp
@@ -0,0 +1,73 @@
+﻿#include "pch.h"
+#include "ServerConfigReader.h"
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <json/json.h>
+#include <room/room_messages.pb.h>
+
+/*-----------------------
+   ServerConfigReader
+-----------------------*/
+
+ServerConfigReader g_ConfigReader;
+
+bool ServerConfigReader::LoadFromFile(const std::string& filePath)
+{
+   std::ifstream file{filePath};
+   if (not file.is_open())
+   {
+	   consoleLogger->Log(Color::Red, L"[Config] Failed to open file: %S\n", filePath.c_str());
+      return false;
+   }
+   
+   std::stringstream buffer;
+   buffer << file.rdbuf();
+   
+   if (not ParseJsonText(buffer.str()))
+   {
+      consoleLogger->Log(Color::Red, L"[Config] Failed to parse config file: %S\n", filePath.c_str());
+      return false;
+   }
+   
+   loadedFilePath_ = filePath;
+   return true;
+}
+
+bool ServerConfigReader::Reload()
+{
+   if (loadedFilePath_.empty()) return false;
+   
+   return LoadFromFile(loadedFilePath_);
+}
+
+bool ServerConfigReader::ParseJsonText(const std::string& jsonText)
+{
+   Json::CharReaderBuilder builder;
+   Json::Value root;
+   std::string errs;
+   
+   std::istringstream iss(jsonText);
+   
+   if (not Json::parseFromStream(builder, iss, &root, &errs))
+   {
+      consoleLogger->Log(Color::Red, L"[Config] JSON parsing error: %S\n", errs.c_str());
+      return false;
+   }
+   
+   // network
+   if (root.isMember("network"))
+   {
+      const Json::Value& network = root["network"];
+      
+      if (network.isMember("bind_ip"))
+         config_.network.bindIp = network["bind_ip"].asString();
+      
+      if (network.isMember("game_port"))
+         config_.network.gamePort = network["game_port"].asInt();
+      
+      if (network.isMember("login_port"))
+         config_.network.loginPort = network["login_port"].asInt();
+   }
+}
+
---
+++ TimeThiefServer/Network/ServerConfigReader.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+#include <string>
+#include "ServerConfig.h"
+
+/*-----------------------
+   ServerConfigReader
+-----------------------*/
+//
+// ServerConfigReader는 서버 설정 파일을 읽어와 ServerConfig 객체로 변환하는 역할을 합니다.
+//
+
+class ServerConfigReader
+{
+public:
+   bool LoadFromFile(const std::string& filePath);
+   bool Reload();
+   
+   const ServerConfig& Get() const { return config_; }
+   
+private:
+   bool ParseJsonText(const std::string& jsonText);
+   
+private:
+   std::string loadedFilePath_;
+   ServerConfig config_;
+    
+};
+
+extern ServerConfigReader g_ConfigReader;
