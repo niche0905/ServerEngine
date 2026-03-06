193d1c464450511dc6da2d774eddb5f9a66684f6
niche0905 <yudle537@gmail.com>
2026-03-06 17:19:56 +0900

    ProtocolShared config 값 추가
    json 문법으로 config 값 몇 개를 추가하고 이를 struct로 준비해 두었다

---
+++ TimeThiefServer/Network/ServerConfig.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include <string>
+
+struct NetworkConfig
+{
+    std::string bindIp = "0.0.0.0";
+    uint16 gamePort = 8252;
+    uint16 loginPort = 8282;
+};
+
+struct ProtocolConfig
+{
+    int32 version = 1;
+};
+
+struct ServerConfig
+{
+    NetworkConfig network;
+    ProtocolConfig protocol;
+};
