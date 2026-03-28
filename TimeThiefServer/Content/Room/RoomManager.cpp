8ef499e7c903c5bd349ee28ad1c6eb97fc73463d
niche0905 <yudle537@gmail.com>
2026-03-28 21:41:44 +0900

    Room Manager 작성
    Room들을 다루는 Container가 될 Room Manager를 작성하였다

---
+++ TimeThiefServer/Content/Room/RoomManager.cpp
@@ -0,0 +1,71 @@
+﻿#include "pch.h"
+#include "RoomManager.h"
+#include "Room.h"
+
+/*----------------
+   RoomManager
+----------------*/
+
+RoomManager::RoomRef RoomManager::CreateRoom(RoomId roomId)
+{
+   auto room = std::make_shared<Room>(roomId);
+   
+   {
+      std::lock_guard lock(mutex_);
+      auto [it, inserted] = rooms_.emplace(roomId, room);
+      if (!inserted) {
+         return nullptr;
+      }
+   }
+   
+   return room;
+}
+
+bool RoomManager::RemoveRoom(RoomId roomId)
+{
+   std::lock_guard lock(mutex_);
+   return rooms_.erase(roomId) > 0;
+}
+
+RoomManager::RoomRef RoomManager::FindRoom(RoomId roomId) const
+{
+   std::lock_guard lock(mutex_);
+   
+   auto it = rooms_.find(roomId);
+   if (it == rooms_.end()) {
+      return nullptr;
+   }
+   
+   return it->second;
+}
+
+size_t RoomManager::GetRoomCount() const
+{
+   std::lock_guard lock(mutex_);
+   return rooms_.size();
+}
+
+std::vector<RoomManager::RoomRef> RoomManager::GetRoomSnapshot() const
+{
+   std::vector<RoomRef> snapshot; 
+   
+   std::lock_guard lock(mutex_);
+   snapshot.reserve(rooms_.size());
+   
+   for (const auto& [roomId, room] : rooms_) {
+      snapshot.push_back(room);
+   }
+   
+   return snapshot;
+}
+
+void RoomManager::Clear()
+{
+   std::lock_guard lock(mutex_);
+   rooms_.clear();
+}
+
+RoomId RoomManager::GenerateUniqueRoomId()
+{
+   return nextRoomId_.fetch_add(1, std::memory_order_relaxed);
+}
---
+++ TimeThiefServer/Content/Room/RoomManager.h
@@ -0,0 +1,47 @@
+﻿#pragma once
+#include <atomic>
+#include <memory>
+#include <mutex>
+#include <unordered_map>
+#include <vector>
+#include "Content/Player/Player.h"
+
+class Room;
+
+/*----------------
+   RoomManager
+----------------*/
+//
+// RoomManager는 게임의 방(Room)을 관리하는 클래스입니다. 각 방은 플레이어들이 모여서 게임을 진행하는 공간입니다.
+//
+
+class RoomManager
+{
+public:
+   using RoomRef = std::shared_ptr<Room>;
+   
+public:
+   RoomManager() = default;
+   ~RoomManager() = default;
+   
+public:
+   RoomRef CreateRoom(RoomId roomId);
+   
+   bool RemoveRoom(RoomId roomId);
+   RoomRef FindRoom(RoomId roomId) const;
+   
+   size_t GetRoomCount() const;
+   
+   std::vector<RoomRef> GetRoomSnapshot() const;
+   
+   void Clear();
+   
+private:
+   RoomId GenerateUniqueRoomId();
+   
+private:
+   mutable std::mutex mutex_;
+   std::unordered_map<RoomId, RoomRef> rooms_;
+   std::atomic<RoomId> nextRoomId_;
+    
+};
