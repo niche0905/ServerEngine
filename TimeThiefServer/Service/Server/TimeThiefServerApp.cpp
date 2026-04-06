8809cedec08fb4cd74794cdd6a58bcd028400a56
niche0905 <yudle537@gmail.com>
2026-04-06 12:18:09 +0900

    ServerApp class 초안 작성
    main에서 굴릴 객체를 Server App (Network IO 제외)의 책임을 가질 객체를 디자인하였다

---
+++ TimeThiefServer/Service/Server/TimeThiefServerApp.cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+#include "TimeThiefServerApp.h"
+
+/*----------------------
+   TimeThiefServerApp
+----------------------*/
+
+bool TimeThiefServerApp::Init(int argc, char* argv[])
+{
+   return false;
+}
+
+bool TimeThiefServerApp::Start()
+{
+   return false;
+}
+
+void TimeThiefServerApp::Run()
+{
+}
+
+void TimeThiefServerApp::Shutdown()
+{
+}
---
+++ TimeThiefServer/Service/Server/TimeThiefServerApp.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+#include "Core/Thread/ThreadManager.h"
+
+class MatchMaker;
+
+/*----------------------
+   TimeThiefServerApp
+----------------------*/
+//
+// TimeThiefServerApp는 TimeThiefServer의 핵심 애플리케이션 클래스입니다.
+//
+
+
+class TimeThiefServerApp
+{
+public:
+   bool Init(int argc, char* argv[]);
+   bool Start();
+   void Run();
+   void Shutdown();
+   
+private:
+   std::shared_ptr<IocpServerService> networkService_;
+   // ShardManager... TODO!
+   // RoomDirectory... TODO! (Room을 Shard에 등록할 수 있고, RoomId를 통해 ShardId를 찾을 수 있는 형태로)
+   std::unique_ptr<MatchMaker> matchMaker_;
+   ThreadManager threadManager_;
+   
+   std::atomic<bool> running_;
+    
+};
