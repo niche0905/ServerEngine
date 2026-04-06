645f6b9e983166f5dd8c86515c4d68a8c1f3cc5f
niche0905 <yudle537@gmail.com>
2026-04-06 19:01:52 +0900

    ShardRoomManager 작성
    Shard에 배치될 RoomManager를 작성을 진행 하였다

---
+++ TimeThiefServer/Service/Room/ShardRoomManager.cpp
@@ -0,0 +1,56 @@
+﻿#include "pch.h"
+#include "ShardRoomManager.h"
+#include "Room.h"
+
+/*--------------------
+   ShardRoomManager
+--------------------*/
+
+ShardRoomManager::RoomRef ShardRoomManager::CreateRoom(RoomId roomId)
+{
+   auto room = Room::Create(roomId);
+   
+   auto [it, inserted] = rooms_.emplace(roomId, room);
+   if (!inserted) {
+      return nullptr;
+   }
+   
+   return room;
+}
+
+bool ShardRoomManager::RemoveRoom(RoomId roomId)
+{
+   return rooms_.erase(roomId) > 0;
+}
+
+ShardRoomManager::RoomRef ShardRoomManager::FindRoom(RoomId roomId) const
+{
+   auto it = rooms_.find(roomId);
+   if (it == rooms_.end()) {
+      return nullptr;
+   }
+   
+   return it->second;
+}
+
+size_t ShardRoomManager::GetRoomCount() const
+{
+   return rooms_.size();
+}
+
+std::vector<ShardRoomManager::RoomRef> ShardRoomManager::GetRoomSnapshot() const
+{
+   std::vector<RoomRef> snapshot;
+   snapshot.reserve(rooms_.size());
+   
+   for (const auto& [roomId, room] : rooms_) {
+      snapshot.push_back(room);
+   }
+   
+   return snapshot;
+}
+
+void ShardRoomManager::Clear()
+{
+   rooms_.clear();
+}
---
+++ TimeThiefServer/Service/Room/ShardRoomManager.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+
+class Room;
+
+/*--------------------
+   ShardRoomManager
+--------------------*/
+//
+// ShardRoomManager는 Shard 내에서 Room을 관리하는 클래스입니다.
+// Shard 기반이므로 MultiThread 환경에서 Data Race가 없을 것 이므로 lock을 사용하지 않습니다.
+//
+
+class ShardRoomManager
+{
+public:
+   using RoomRef = std::shared_ptr<Room>;
+   
+public:
+   ShardRoomManager() = default;
+   ~ShardRoomManager() = default;
+   
+   ShardRoomManager(ShardRoomManager const&) = delete;
+   ShardRoomManager& operator=(ShardRoomManager const&) = delete;
+   
+public:
+   RoomRef CreateRoom(RoomId roomId);
+   
+   bool RemoveRoom(RoomId roomId);
+   RoomRef FindRoom(RoomId roomId) const;
+   
+   size_t GetRoomCount() const;
+   
+   std::vector<RoomRef> GetRoomSnapshot() const;
+   
+   void Clear();
+   
+private:
+   std::unordered_map<RoomId, RoomRef> rooms_;
+    
+};
