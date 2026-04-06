efc64e460640d1e0352fb24aa127374ddd6203d1
niche0905 <yudle537@gmail.com>
2026-04-06 19:22:35 +0900

    RoomDirectory 작성
    RoomDirectory를 작성해서 전역으로 현재 생성된 Room과 Shard 간의 연관을 지닐 class를 작성하였다

---
+++ TimeThiefServer/Shard/RoomDirectory.cpp
@@ -0,0 +1,41 @@
+﻿#include "pch.h"
+#include "RoomDirectory.h"
+
+/*-----------------
+   RoomDirectory
+-----------------*/
+
+bool RoomDirectory::RegisterRoom(RoomId roomId, ShardId shardId)
+{
+   if (roomId == 0)
+      return false;
+   
+   std::unique_lock lock(mutex_);
+   
+   auto [it, inserted] = roomToShard_.emplace(roomId, shardId);
+   return inserted;
+}
+
+bool RoomDirectory::UnregisterRoom(RoomId roomId)
+{
+   if (roomId == 0)
+      return false;
+   
+   std::unique_lock lock(mutex_);
+   
+   return roomToShard_.erase(roomId) > 0;
+}
+
+std::optional<ShardId> RoomDirectory::FindShardId(RoomId roomId) const
+{
+   if (roomId == 0)
+      return std::nullopt;
+   
+   std::shared_lock lock(mutex_);
+   
+   auto it = roomToShard_.find(roomId);
+   if (it == roomToShard_.end())
+      return std::nullopt;
+   
+   return it->second;
+}
---
+++ TimeThiefServer/Shard/RoomDirectory.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include <optional>
+#include <shared_mutex>
+#include <unordered_map>
+
+/*-----------------
+   RoomDirectory
+-----------------*/
+//
+// RoomDirectory는 Room에 맞는 Shard를 찾아주는 역할을 합니다.
+//
+
+class RoomDirectory
+{
+public:
+   bool RegisterRoom(RoomId roomId, ShardId shardId);
+   bool UnregisterRoom(RoomId roomId);
+   std::optional<ShardId> FindShardId(RoomId roomId) const;
+   
+private:
+   mutable std::shared_mutex mutex_;
+   std::unordered_map<RoomId, ShardId> roomToShard_;
+    
+};
