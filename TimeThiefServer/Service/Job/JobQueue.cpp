c5da5195e5f6b9cafbb0b6b7a06a3ef70747ad52
niche0905 <yudle537@gmail.com>
2026-04-08 12:01:55 +0900

    JobQueue 작성
    간단하게 mutex 이용한 JobQueue를 작성하였다

---
+++ TimeThiefServer/Service/Job/JobQueue.cpp
@@ -0,0 +1,40 @@
+﻿#include "pch.h"
+#include "JobQueue.h"
+
+/*------------
+   JobQueue
+------------*/
+
+bool JobQueue::Push(Job job)
+{
+   if (!job) return false;
+   
+   {
+      std::lock_guard<std::mutex> lock(mutex_);
+      queue_.push(std::move(job));
+   }
+   
+   return true;
+}
+
+bool JobQueue::TryPop(Job& outJob)
+{
+   std::lock_guard<std::mutex> lock(mutex_);
+   if (queue_.empty()) return false;
+   
+   outJob = std::move(queue_.front());
+   queue_.pop();
+   return true;
+}
+
+size_t JobQueue::DrainTo(std::vector<Job>& outJobs)
+{
+   std::lock_guard<std::mutex> lock(mutex_);
+   size_t count = 0;
+   while (!queue_.empty()) {
+      outJobs.push_back(std::move(queue_.front()));
+      queue_.pop();
+      ++count;
+   }
+   return count;
+}
---
+++ TimeThiefServer/Service/Job/JobQueue.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+/*------------
+   JobQueue
+------------*/
+//
+// JobQueue는 서버에서 실행되어야 하는 작업들을 관리하는 큐입니다.
+// MPSC (Multi-Producer, Single-Consumer) 방식으로 구현을 고려하며
+// 현재는 Lock을 통해서 구현할 예정이지만, 추후에 Lock-Free 방식으로 개선할 수 있습니다.
+//
+
+class JobQueue
+{
+public:
+   bool Push(Job job);
+   bool TryPop(Job& outJob);
+   
+   size_t DrainTo(std::vector<Job>& outJobs);
+   
+private:
+   std::mutex mutex_;
+   std::queue<Job> queue_;
+    
+};
