eb1e1f517c82f22db94c39e4e42967d676a52f3c
niche0905 <yudle537@gmail.com>
2026-04-09 14:59:24 +0900

    TimerQueue 작성
    Timer Queue 를 작성하였다
    Shard 마다 배치되어 경합이 없을 것이라 상정하고 구현하였다

---
+++ TimeThiefServer/Service/Timer/TimerQueue.cpp
@@ -0,0 +1,83 @@
+﻿#include "pch.h"
+#include "TimerQueue.h"
+#include "TimerTask.h"
+
+/*--------------
+   TimerQueue
+--------------*/
+
+TimerId TimerQueue::ScheduleAt(TimePoint executeAt, Job callback)
+{
+   if (!callback)
+      return 0;
+   
+   TimerId timerId = nextTimerId_++;
+   heap_.push(TimerTask{ executeAt, timerId, std::move(callback) });
+   
+   return timerId;
+}
+
+TimerId TimerQueue::ScheduleAfter(Duration delay, Job callback)
+{
+   if (!callback)
+      return 0;
+   
+   return ScheduleAt(Clock::now() + delay, std::move(callback));
+}
+
+bool TimerQueue::Cancel(TimerId timerId)
+// lazy cancel 이므로 이미 실행된 Timer의 경우도 true로 반환될 수 있음
+{
+   if (timerId == 0)
+      return false;
+   
+   cancelled_.insert(timerId);
+   return true;
+}
+
+void TimerQueue::PopExpired(TimePoint now, std::vector<TimerTask>& outJobs)
+{
+   outJobs.clear();
+   
+   while (not heap_.empty()) {
+      const TimerTask& top = heap_.top();
+      if (top.executeAt > now)
+         break;
+      
+      TimerTask task = top;   // 복사 허용..
+      heap_.pop();
+      
+      if (cancelled_.erase(task.timerId) > 0) {
+         continue;
+      }
+      
+      outJobs.push_back(std::move(task));
+   }
+}
+
+bool TimerQueue::Empty() const
+{
+   return heap_.empty();
+}
+
+size_t TimerQueue::Size() const
+{
+   return heap_.size();
+}
+
+std::optional<TimePoint> TimerQueue::GetNextExecuteAt() const
+{
+   if (heap_.empty())
+      return std::nullopt;
+   
+   return heap_.top().executeAt;
+}
+
+void TimerQueue::Clear()
+{
+   while (!heap_.empty()) {
+      heap_.pop();
+   }
+   
+   cancelled_.clear();
+}
---
+++ TimeThiefServer/Service/Timer/TimerQueue.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+
+struct TimerTaskCompare;
+struct TimerTask;
+
+/*--------------
+   TimerQueue
+--------------*/
+//
+// TimerQueue는 타이머 이벤트를 관리하는 클래스입니다.
+// Shard에 배치되어 Data Race가 없는 상황을 상정하고 만들어 졌습니다.
+// 따라서 Shard가 아닌 다른 곳에서 TimerQueue에 접근할 때는 반드시 Shard의 Job Queue에 작업을 넣어서 접근해야 합니다.
+//
+
+class TimerQueue
+{
+public:
+   TimerQueue() = default;
+   
+   TimerId ScheduleAt(TimePoint executeAt, Job callback);
+   TimerId ScheduleAfter(Duration delay, Job callback);
+   
+   bool Cancel(TimerId timerId);
+   
+   void PopExpired(TimePoint now, std::vector<TimerTask>& outJobs);
+   
+   bool Empty() const;
+   size_t Size() const;
+   
+   std::optional<TimePoint> GetNextExecuteAt() const;
+   
+   void Clear();
+   
+private:
+   std::priority_queue<TimerTask, std::vector<TimerTask>, TimerTaskCompare> heap_;
+   std::unordered_set<TimerId> cancelled_;
+   TimerId nextTimerId_ = 1;
+    
+};
