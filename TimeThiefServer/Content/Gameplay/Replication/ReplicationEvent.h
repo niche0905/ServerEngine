596a215cbd70cbb61ff3024dccb3304e19a25422
niche0905 <yudle537@gmail.com>
2026-04-16 15:08:36 +0900

    Rep Event 확장 및 일반화
    Rep Event만으로 부족하다는 것을 느껴서 일반화 된 RepEvent들을 선언하였다

--- TimeThiefServer/Content/Gameplay/Replication/ReplicationEvent.h
+++ TimeThiefServer/Content/Gameplay/Replication/ReplicationEvent.h
@@ -2,6 +2,9 @@
 #include "Content/Object/ObjectId.h"
 #include "ReplicationTypes.h"
 #include <vector>
+#include <common/common_types.pb.h>
+
+#include "Physics/Narrowphase/IntersectUtil.h"
 
 enum class RepEventType : uint8
 {
@@ -10,31 +13,118 @@ enum class RepEventType : uint8
     Spawn,
     Despawn,
     
-    Damage,
-    Death,
-    Respawn,
-    
-    Fire,
-    ProjectileExplode,
-    
-    ItemUseStart,
-    ItemUseCancel,
-    ItemUseComplete,
-    
-    AttackStart,
-    AttackHit,
+    // Damage,
+    // Death,
+    // Respawn,
+    //
+    // Fire,
+    // ProjectileExplode,
+    //
+    // ItemUseStart,
+    // ItemUseCancel,
+    // ItemUseComplete,
+    //
+    // AttackStart,
+    // AttackHit,
 };
 
 struct RepEvent
 {
     RepEventType type{RepEventType::None};
     ObjectId source{};
-    ObjectId target{};
-    
-    // 추가 데이터
-    int32 a{0};
-    int32 b{0};
-    
+    TickSeq tick{0};
+    uint64 timeMs{0};
+};
+
+struct RepFireEvent
+{
+    ObjectId source{};
+    uint32 weaponId{0};
+    uint32 shotSeed{0};
+    SE::Math::Vector3 origin{};
+    SE::Math::Vector3 direction{};
+    TickSeq tick{0};
+    uint64 timeMs{0};
+};
+
+struct RepThrowEvent
+{
+    ObjectId source{};
+    uint32 grenadeType{0};
+    SE::Math::Vector3 origin{};
+    SE::Math::Vector3 direction{};
+    TickSeq tick{0};
+    uint64 timeMs{0};
+};
+
+struct RepReloadEvent
+{
+    ObjectId source{};
+    uint32 weaponId{0};
+    TickSeq tick{0};
+    uint64 timeMs{0};
+};
+
+struct RepHitEvent
+{
+    ObjectId source{};
+    se::common::Vector3 hitPoint{};
+    TickSeq tick{0};
+    uint64 timeMs{0};
+};
+
+// struct RepUseAbilityEvent
+// {
+//     ObjectId source{};
+//     uint32 abilityId{0};
+//     TickSeq tick{0};
+//     uint64 timeMs{0};
+// };
+
+// struct RepUseItemEvent
+// {
+//     ObjectId source{};
+//     uint32 itemId{0};
+//     TickSeq tick{0};
+//     uint64 timeMs{0};
+// };
+
+// struct RepChestEvent
+// {
+//     ObjectId source{};
+//     ObjectId chestId{0};
+//     TickSeq tick{0};
+//     uint64 timeMs{0};
+// };
+
+// struct RepPickupEvent
+// {
+//     ObjectId source{};
+//     ObjectId itemId{0};
+//     TickSeq tick{0};
+//     uint64 timeMs{0};
+// };
+
+struct RepWireLaunchEvent
+{
+    ObjectId source{};
+    SE::Math::Vector3 origin{};
+    SE::Math::Vector3 direction{};
+    TickSeq tick{0};
+    uint64 timeMs{0};
+};
+
+struct RepWireEvent
+{
+    ObjectId source{};
+    SE::Math::Vector3 anchorPos{};
+    TickSeq tick{0};
+    uint64 timeMs{0};
+};
+
+struct RepWireEndEvent
+{
+    ObjectId source{};
     TickSeq tick{0};
     uint64 timeMs{0};
 };
