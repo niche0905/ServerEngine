e312d0883ed651c8f0d6a238f092b5b4f6201352
niche0905 <yudle537@gmail.com>
2026-04-24 17:50:23 +0900

    MonsterReplicator 정의
    Monster Replicator의 정의 부분을 작성하였다

---
+++ TimeThiefServer/Content/Gameplay/Replication/Replicator/MonsterReplicator.cpp
@@ -0,0 +1,25 @@
+﻿#include "pch.h"
+#include "MonsterReplicator.h"
+#include "Content/Object/Actor/MonsterPawn.h"
+#include "Service/Room/Room.h"
+
+
+ReplicateResult MonsterReplicator::FlushPeriodic(BaseObject* obj, ReplicationDirty flags, const RepFrame& frame,
+    uint64 nowMs, Room& room) const
+{
+    auto* monster = dynamic_cast<MonsterPawn*>(obj);
+    if (!monster)
+        return ReplicateResult{};
+    
+    return FlushMonsterPeriodic(*monster, flags, frame, nowMs, room);
+}
+
+ReplicateResult MonsterReplicator::FlushMonsterPeriodic(MonsterPawn& monster, ReplicationDirty flags,
+    const RepFrame& frame, uint64 nowMs, Room& room) const
+{
+    ReplicateResult result;
+    
+    // TODO: Monster Pawn과 BT 구조가 이루어지면 Replicate 대상을 다루도록
+    
+    return result;
+}
---
+++ TimeThiefServer/Content/Gameplay/Replication/Replicator/MonsterReplicator.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include "Content/Gameplay/Replication/IObjectReplicator.h"
+
+class MonsterPawn;
+
+class MonsterReplicator : public IObjectReplicator
+{
+public:
+    virtual ReplicateResult FlushPeriodic(BaseObject* obj, ReplicationDirty flags, const RepFrame& frame, uint64 nowMs, Room& room) const override;
+    
+private:
+    ReplicateResult FlushMonsterPeriodic(MonsterPawn& monster, ReplicationDirty flags, const RepFrame& frame, uint64 nowMs, Room& room) const;
+    
+};
