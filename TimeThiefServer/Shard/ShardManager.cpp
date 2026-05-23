cb8d9aa2abaa02c92735d49b808f555a9ae3a0e8
niche0905 <yudle537@gmail.com>
2026-05-23 21:20:48 +0900

    Select Shard Load Balancing (P2C) 예시
    P2C (Power of Two Choices) 방법을 사용하는 방식의 기법을 더미 코드로 작성함

--- TimeThiefServer/Shard/ShardManager.cpp
+++ TimeThiefServer/Shard/ShardManager.cpp
@@ -1,5 +1,6 @@
 ﻿#include "pch.h"
 #include "ShardManager.h"
+#include <random>
 #include "GameShard.h"
 #include "RoomDirectory.h"
 #include "Core/Thread/ThreadManager.h"
@@ -105,6 +106,29 @@ ShardId ShardManager::SelectShardForNewRoom()
    
    uint32 index = nextShard_.fetch_add(1, std::memory_order_relaxed) % shards_.size();
    return static_cast<ShardId>(index + 1);
+   
+   // P2C (Power of Two Choices)
+   {
+      const uint32 shardCount = static_cast<uint32>(shards_.size());
+      
+      static thread_local std::mt19937 rng(std::random_device{}());
+      std::uniform_int_distribution<uint32> dist(0, shardCount - 1);
+      
+      uint32 a = dist(rng);
+      uint32 b = dist(rng);
+      
+      if (a == b) {
+         b = (b + 1) % shardCount;  // 같은 Shard가 선택된 경우, 다음 Shard로 선택
+      }
+      
+      // 아래 코드는 당장 사용할 수 없음 (GetRoomCount는 Multi Thread를 고려하지 않았기에 정상 작동 불가함)
+      // 사용하고자 하면 atomic 한 counter를 두고 그 값을 사용하여야 함 (Thread Safe하게)
+      size_t sizeA = shards_[a]->GetRoomCount();
+      size_t sizeB = shards_[b]->GetRoomCount();
+      
+      uint32 selected = (sizeA <= sizeB) ? a : b;
+      return static_cast<ShardId>(selected + 1);
+   }
 }
 
 int32 ShardManager::GetShardCount() const
