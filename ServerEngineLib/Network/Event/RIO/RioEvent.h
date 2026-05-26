e8e609c7f354e1c11a516ae786a8472cf8813337
niche0905 <yudle537@gmail.com>
2026-05-26 22:07:44 +0900

    Rio Event 잓어
    기본 Rio Event의 변경 사항과 Io Event 관련 작성을 진행하였다

--- ServerEngineLib/Network/Event/RIO/RioEvent.h
+++ ServerEngineLib/Network/Event/RIO/RioEvent.h
@@ -15,19 +15,15 @@ public:
 	virtual ~RioEvent() = default;
 
 	virtual IoEventType GetType() const noexcept override { return type_; }
+	
 	virtual void SetOwner(std::shared_ptr<IoObject> owner) override { owner_ = owner; }
 	virtual std::shared_ptr<IoObject> GetOwner() const override { return owner_; }
 
 	void* GetNativeContext() noexcept override { return this; }
 
-	RIO_BUF* GetRioBuffer() { return &rioBuffer_; }
-	void SetResult(RIORESULT* result) { context_ = result; }
-
 private:
 	IoEventType					type_{ IoEventType::None };
 	std::shared_ptr<IoObject>	owner_{ nullptr };
-	RIO_BUF						rioBuffer_{};
-	RIORESULT*					context_{ nullptr };
 
 };
 
---
+++ ServerEngineLib/Network/Event/RIO/RioIoEvent.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+#include "RioEvent.h"
+
+class RioSendBuffer;
+
+/*----------------
+   RioRecvEvent
+----------------*/
+
+class RioRecvEvent : public RioEvent
+{
+public:
+    RioRecvEvent()
+        : RioEvent(IoEventType::Recv)
+    {
+
+    }
+    
+};
+
+/*----------------
+   RioSendEvent
+----------------*/
+
+class RioSendEvent : public RioEvent
+{
+public:
+    RioSendEvent()
+        : RioEvent(IoEventType::Send)
+    {
+
+    }
+    
+public:
+    std::vector<std::shared_ptr<RioSendBuffer>> sendBuffers_;
+    std::vector<RIO_BUF> rioBuffers_;
+    
+};
