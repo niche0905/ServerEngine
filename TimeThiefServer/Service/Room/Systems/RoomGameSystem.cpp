2eeeb4e795dc3f9276d72c9a1b7cdd8bcf07c60e
Niche <73772820+niche0905@users.noreply.github.com>
2026-07-22 20:31:58 +0900

    클라이언트 맵&배치 정보 변경 데이터 적용
    클라이언트에서 수정한 Map 데이터와 해당 Map에 맞는 배치, 그리고 몬스터 Collision 변경을 추출하여 적용하였다

--- TimeThiefServer/Service/Room/Systems/RoomGameSystem.cpp
+++ TimeThiefServer/Service/Room/Systems/RoomGameSystem.cpp
@@ -19,7 +19,7 @@ bool RoomGameSystem::Init(Room* ownerRoom, const GameDataManager& gameDataManage
    gameDataManager_ = &gameDataManager;
    
    // TEMP: 아래 ZoneBounds 값은 나중에 제대로 바꾸어야 함 (Map Data를 읽던, 하드 코딩으로 Map min, max를 넣던)
-   if (!zoneSystem_.Init(ownerRoom, ZoneBounds{ SE::Math::Vector3{}, SE::Math::Vector3{130740.0, 160330.0, 0.0f} }, gameDataManager_->GetZoneTable(), gameConfig.zoneDamageTickInterval))
+   if (!zoneSystem_.Init(ownerRoom, ZoneBounds{ SE::Math::Vector3{}, SE::Math::Vector3{110000.0, 110000.0, 0.0f} }, gameDataManager_->GetZoneTable(), gameConfig.zoneDamageTickInterval))
       return false;
    
    if (!respawnSystem_.Init(ownerRoom))
